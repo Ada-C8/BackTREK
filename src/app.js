777848c196d8cc0d1924af0aae108293967b72bc
Gale Harrington <gale.harrington19@gmail.com>
2017-11-28 15:19:26 -0800

    Trip list doesn't show until you click on Explore Trips

--- src/app.js
+++ src/app.js
@@ -14,15 +14,17 @@ const tripList = new TripList();
 let tripTemplate;
 
 const render = function render(tripList) {
-  $('#trip-table').empty(); // clear it so it doesn't continually add on
+  $('#trip-list').empty(); // clear it so it doesn't continually add on
   tripList.forEach((trip) => {
     // use template to append trip row to table in the DOM
-    $('#trip-table').append(tripTemplate(trip.attributes));
+    $('#trip-list').append(tripTemplate(trip.attributes));
   });
 };
 
 $(document).ready( () => {
   tripTemplate = _.template($('#trip-row-template').html());
+
+  $('#explore-trips').on('click', function() {$('#trip-table').show()});
 
   tripList.on('update', render, tripList)
 
