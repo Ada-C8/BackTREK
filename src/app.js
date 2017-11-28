9fd5d5b7977deca52c9d3d31ed9f7148c2f4bc5c
Tanisha <tanishahampden@gmail.com>
2017-11-28 15:36:14 -0800

    set up app.js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,14 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+import TripList from 'app/collections/trip_list';
+
+const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'cost'];
+
+const tripList = new TripList();
+
 console.log('it loaded!');
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  // $('main').html('<h1>Hello World!</h1>');
 });
