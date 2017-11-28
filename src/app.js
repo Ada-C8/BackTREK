9ccc1c42af49c01edea119cb58974e366e063931
Andrea Chen <andreachen.ac@gmail.com>
2017-11-28 15:38:38 -0800

    First commit. Create trip model

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+import Trip from './models/trip'
+
 console.log('it loaded!');
+
+const ALL_TRIPS_URL = 'https://ada-backtrek-api.herokuapp.com'; 
 
 $(document).ready( () => {
   $('main').html('<h1>Hello World!</h1>');
