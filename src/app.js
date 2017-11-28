1f53c6660d9341c9363216d42dcb2f4b79624e52
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-28 14:55:23 -0800

    Add trip list collection file and initial changes to app and index based on past projects

--- src/app.js
+++ src/app.js
@@ -6,8 +6,35 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-console.log('it loaded!');
+import TripList from './collections/trip_list';
+import Trip from './models/trip';
+
+
+const rawTripData = [
+  {
+    name: "Cairo to Zanzibar",
+    continent: "Africa",
+    about: 'So Awesome!',
+    weeks: 5,
+    cost: 9599.99
+  }, {
+    name: "Espana",
+    continent: "Europe",
+    about: "Motherland!",
+    weeks: 6,
+    cost: 'free'
+  }
+]
+
+const tripList = new TripList(rawTripData);
+
+let tripTemplate;
+
+let tripsTemplate;
+
 
 $(document).ready( () => {
   $('main').html('<h1>Hello World!</h1>');
+
+  tripTemplate = _.template($('#trip-template').html());
 });
