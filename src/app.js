3a6cd6144468e58216ca6494ef265c2515ec942a
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-28 14:23:12 -0800

    Load all trips in a table when user clicks 'All Trips' link

--- src/app.js
+++ src/app.js
@@ -6,8 +6,27 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-console.log('it loaded!');
+let allTripsTemplate;
+let tripHeadersTemplate;
+
+const fields = ['id', 'name', 'continent', 'category', 'weeks', 'cost'];
+
+const loadTrips = function loadTrips() {
+  $('#all-trips').empty();
+  $('#trip-headers').empty();
+  const url = 'https://ada-backtrek-api.herokuapp.com/trips';
+  $.get(url, (response) => {
+    fields.forEach((field) => {
+      $('#trip-headers').append(tripHeadersTemplate({header: field}))
+    });
+    response.forEach((trip) => {
+      $('#all-trips').append(allTripsTemplate(trip))
+    })
+  });
+};
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  allTripsTemplate = _.template($('#all-trips-template').html());
+  tripHeadersTemplate = _.template($('#trip-headers-template').html());
+  $('#load-trips').on('click', loadTrips);
 });
