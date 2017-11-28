83bf6d5f3d70bf2b62fa5898e01048bdacf88763
Tanisha <tanishahampden@gmail.com>
2017-11-28 15:14:09 -0800

    add trips api to collection

---
+++ src/app/collections/trip_list.js
@@ -0,0 +1,12 @@
+import Backbone from 'backbone'
+import Trip from '../models/trip';
+
+const TripList = Backbone.Collection.extend({
+  model: Trip,
+  url: 'https://ada-backtrek-api.herokuapp.com/trips',
+  parse: function(response) {
+    return response['trips'];
+  }
+});
+
+export default TripList;
