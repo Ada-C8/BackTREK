dd6966893fe7eb5457ba81fc49eccca34ce31877
Gale Harrington <gale.harrington19@gmail.com>
2017-11-28 15:02:44 -0800

    Create template for trip rows, get trip list from Travel Api

--- src/app.js
+++ src/app.js
@@ -6,16 +6,25 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-// Models
+// Models and Collections
 import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list';
 
-const paris = new Trip({
-  name: 'Paris Adventure',
-  category: 'Urban',
-  continent: 'Europe',
-  cost: 100
-});
+const tripList = new TripList();
+let tripTemplate;
+
+const render = function render(tripList) {
+  $('#trip-table').empty(); // clear it so it doesn't continually add on
+  tripList.forEach((trip) => {
+    // use template to append trip row to table in the DOM
+    $('#trip-table').append(tripTemplate(trip.attributes));
+  });
+};
 
 $(document).ready( () => {
-  console.log(paris);
+  tripTemplate = _.template($('#trip-row-template').html());
+
+  tripList.on('update', render, tripList)
+
+  tripList.fetch(); // will automatically call render
 });
