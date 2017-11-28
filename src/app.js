20ec1a7c69fb36ba80916d4656a0d24eb66b6a9a
Bennett Rahn <rahnibennett@gmail.com>
2017-11-28 15:48:21 -0800

    add basic table, and api call for all trips

--- src/app.js
+++ src/app.js
@@ -8,10 +8,26 @@ import './css/style.css';
 
 import TripList from './app/collections/trip_list';
 
-
 const tripList = new TripList();
+let tripTemplate;
+
+const render = function render() {
+  const tableElement = $('#trip-list');
+  tableElement.html('');
+
+  tripList.forEach((trip) => {
+    const generatedHTML = tripTemplate(trip.attributes);
+
+    tableElement.append(generatedHTML);
+  });
+};
+
 
 
 $(document).ready( () => {
-  console.log(tripList);
+  tripTemplate = _.template($('#trip-template').html());
+
+  tripList.on('update', render)
+
+  tripList.fetch();
 });
--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -4,11 +4,12 @@ import Trip from '../models/trip'
 const TripList = Backbone.Collection.extend({
   model: Trip,
 
-  // url:'http://localhost:3000/books',
-  // parse: function(response) {
-  //   return response['books'];
-  // },
-  //
+  url:'https://ada-backtrek-api.herokuapp.com/trips',
+  parse: function(response) {
+    console.log(response);
+    return response;
+  },
+
   // comparator: 'title',
 });
 
