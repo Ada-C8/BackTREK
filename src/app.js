92683f1bacac80705dfd43199a5ade6eee13dfed
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-28 15:50:18 -0800

    Able to import data from static data in js file

--- src/app.js
+++ src/app.js
@@ -10,46 +10,48 @@ import TripsList from './app/collections/trips_list.js';
 import Trip from './app/models/trip.js';
 
 
-// const rawTripData = [
-//   {
-//     name: "Cairo to Zanzibar",
-//     continent: "Africa",
-//     about: 'So Awesome!',
-//     weeks: 5,
-//     cost: 9599.99
-//   }, {
-//     name: "Espana",
-//     continent: "Europe",
-//     about: "Motherland!",
-//     weeks: 6,
-//     cost: 'free'
-//   }
-// ]
+const rawTripData = [
+  {
+    name: "Cairo to Zanzibar",
+    continent: "Africa",
+    about: 'So Awesome!',
+    weeks: 5,
+    cost: 9599.99
+  }, {
+    name: "Espana",
+    continent: "Europe",
+    about: "Motherland!",
+    weeks: 6,
+    cost: 'free'
+  }
+]
 
+const tripsList = new TripsList(rawTripData);
 
-const render = function render(tripList) {
-  const tripElement = $('#song-list');
-
-  songList.forEach((song) => {
-    const generatedHTML = $(songTemplate(song.attributes));
-    songElement.append(generatedHTML);
-  });
+const render = function render(tripsList) {
+  const tripTableElement = $('#trips-list');
+  tripsList.forEach((trip) => {
+    const generatedHTML = tripsTemplate(trip.attributes);
+    console.log(generatedHTML);
+    tripTableElement.append(generatedHTML);
+  })
 };
 
 
-const tripsList = new TripsList();
-tripsList.fetch();
+
+// tripsList.fetch();
 
 
-let tripTemplate;
+// let tripTemplate;
 
 let tripsTemplate;
 
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
-
   tripsTemplate = _.template($('#trips-template').html());
 
-  tripTemplate = _.template($('#trip-template').html());
+  // tripTemplate = _.template($('#trip-template').html());
+
+  render(tripsList);
+
 });
