81211ce8ccce62f04d0f19eeecf8e3b68fffc3fa
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-28 15:35:48 -0800

    begin basic HTML template and import model/collection

--- src/app.js
+++ src/app.js
@@ -6,8 +6,35 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-console.log('it loaded!');
+// OUR COMPONENTS
+import TripList from './collections/trip_list';
 
+// TRIP FIELDS
+const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'cost'];
+
+const tripList = new TripList();
+
+// Starts undefined - we'll set this in $(document).ready
+// once we know the template is available
+let tripTemplate;
+
+const render = function render(tripList) {
+  // iterate through the tripList, generate HTML
+  // for each model and attatch it to the DOM
+  const tripTableElement = $('#trip-list');
+  tripTableElement.html('');
+
+  tripList.forEach((trip) => {
+    const generatedHTML = tripTemplate(trip.attributes);
+    tripTableElement.append(generatedHTML);
+  });
+
+  // Provide visual feedback for sorting
+  $('th.sort').removeClass('current-sort-field');
+  $(`th.sort.${ tripList.comparator }`).addClass('current-sort-field');
+};
+
+///////////////////////////
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  tripTemplate = _.template($('#trip-template').html());
 });
