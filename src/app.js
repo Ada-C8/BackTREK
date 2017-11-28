29c4a7f315c7494fc6bcc2625e27811fd457f1a2
Canaan West <canaan.west@gmail.com>
2017-11-28 15:26:02 -0800

    show all working

--- src/app.js
+++ src/app.js
@@ -6,8 +6,38 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list';
+
 console.log('it loaded!');
 
+const tripList = new TripList();
+
+const render = function(tripList) {
+  const tripTemplate = _.template($('#trip-template').html());
+  const $tripList = $('#trip-list');
+  $tripList.empty();
+  tripList.forEach((trip) => {
+    $tripList.append($(tripTemplate(trip.attributes)).attr('id', `${trip.id}`));
+  })
+}
+
+const updateHandler = (list) => {
+
+}
+
+tripList.on('update', updateHandler)
+
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  $('#all-trips').hide();
+  tripList.on('update', render, tripList);
+  tripList.fetch();
+  $('#show-all').on('click', function(){
+    $('#all-trips').show();
+  })
+
+  $('#trip-list').on('click', 'tr', function(){
+    const myId = $(this).attr('id');
+    console.log(myId);
+  })
 });
