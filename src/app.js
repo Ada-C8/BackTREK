734567306895d670510a7dac57b3d372af25549a
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-28 15:31:23 -0800

    Blocking out the table on html

--- src/app.js
+++ src/app.js
@@ -10,21 +10,32 @@ import TripsList from './app/collections/trips_list.js';
 import Trip from './app/models/trip.js';
 
 
-const rawTripData = [
-  {
-    name: "Cairo to Zanzibar",
-    continent: "Africa",
-    about: 'So Awesome!',
-    weeks: 5,
-    cost: 9599.99
-  }, {
-    name: "Espana",
-    continent: "Europe",
-    about: "Motherland!",
-    weeks: 6,
-    cost: 'free'
-  }
-]
+// const rawTripData = [
+//   {
+//     name: "Cairo to Zanzibar",
+//     continent: "Africa",
+//     about: 'So Awesome!',
+//     weeks: 5,
+//     cost: 9599.99
+//   }, {
+//     name: "Espana",
+//     continent: "Europe",
+//     about: "Motherland!",
+//     weeks: 6,
+//     cost: 'free'
+//   }
+// ]
+
+
+const render = function render(tripList) {
+  const tripElement = $('#song-list');
+
+  songList.forEach((song) => {
+    const generatedHTML = $(songTemplate(song.attributes));
+    songElement.append(generatedHTML);
+  });
+};
+
 
 const tripsList = new TripsList();
 tripsList.fetch();
