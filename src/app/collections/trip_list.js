3ebb7e3f470e4964865e7cf4ea3990cabfff98e6
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-28 14:58:36 -0800

    Add url to trips collection

--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -2,7 +2,11 @@ import Backbone from 'backbone';
 import Trip from '../models/trip';
 
 const TripList = Backbone.Collection.extend({
-  model: Trip
+  model: Trip,
+  url: 'https://trektravel.herokuapp.com/trips/',
+ //  parse: function(response) {
+ //   return response['books'];
+ // } // may or may not need this parsing functionality
 });
 
 export default TripList;
