020f7dbe24cd30cdaf1de80470e563d77d397b9e
Jessica Owens <vertige@gmail.com>
2017-11-28 14:41:28 -0800

    transferred over previous Trek html and css

--- src/app.js
+++ src/app.js
@@ -6,8 +6,49 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-console.log('it loaded!');
+// Backbone Models
+import Trip from './app/models/trip';
+
+// Backbone Collections
+import TripList from './app/collections/trip_list';
+
+
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  // PREP
+  $('#reservation-form').hide();
+
+  // BASICS
+  // $('#get-list').click(displayList);
 });
+
+// $(document).ready(() => {
+//
+//   // BASICS
+//   $('#list').on('click', 'li', function fx() {
+//     $('#list-wrapper').addClass('large-5');
+//     displayDetails($(this)[0].id);
+//   });
+//
+//   // FORM
+//   $('form').submit(function fx(e) {
+//     e.preventDefault();
+//     postReservation($(this));
+//   });
+// });
+
+// // Display List of Trips
+// const displayList = function displayList() {
+//   $('.home').removeClass('home');
+//   $('#list').removeClass('hide');
+//   $.get(apiUrl, (response) => {
+//     let list = '<ul>';
+//     response.forEach((thing) => {
+//       list += `<li id="${thing.id}">${thing.name}</li>`;
+//     });
+//     list += '</ul>';
+//     $('#list').html(list);
+//   }).fail(() => {
+//     $('#message').addClass('failure').html('Oops! Something went wrong!');
+//   });
+// };
--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -1,7 +1,7 @@
 import Backbone from 'backbone';
 import Trip from '../models/trip';
 
-const TripList = BackBone.Collection.extend({
+const TripList = Backbone.Collection.extend({
   model: Trip,
   url: 'https://ada-backtrek-api.herokuapp.com/trips',
   parse(response) {
