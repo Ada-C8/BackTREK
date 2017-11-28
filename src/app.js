10750ebe19f284441d3057e9d346c98b42425a0a
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-28 15:22:37 -0800

    Dynamically add form when user clicks 'Add trip' link

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const tripList = new TripList();
 let allTripsTemplate;
 let tripHeadersTemplate;
 let showTripTemplate;
+let addTripTemplate;
 
 const url = 'https://ada-backtrek-api.herokuapp.com/trips';
 const fields = ['id', 'name', 'continent', 'category', 'weeks', 'cost'];
@@ -42,11 +43,27 @@ const showTrip = function showTrip(event) {
   })
 };
 
+const addTripForm = function addTripForm() {
+  $('.content').empty();
+  fields.forEach((item) => {
+    $('#add-trip-form').append(addTripTemplate({field: item, lowercaseField: item}));
+  });
+  $('#add-trip-form').append('<section><button type="submit" class="button">Submit</button></section></form>');
+}
+
+const saveTrip = function saveTrip(event) {
+  event.preventDefault();
+  // TODO: save the trip to the API
+}
+
 $(document).ready( () => {
   allTripsTemplate = _.template($('#all-trips-template').html());
   tripHeadersTemplate = _.template($('#trip-headers-template').html());
   showTripTemplate = _.template($('#show-trip-template').html());
+  addTripTemplate = _.template($('#add-trip-template').html());
   $('#load-trips').on('click', loadTrips);
   $('#all-trips').on('click', '.trip', showTrip);
+  $('#add-trip').on('click', addTripForm);
+  $('#add-trip-form').on('submit', saveTrip);
   tripList.fetch();
 });
