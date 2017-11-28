71550803fac16f3cb65c398c27fbf0d2f2509843
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-11-28 14:15:10 -0800

    Working api parsing, not appending to element.

--- src/app.js
+++ src/app.js
@@ -12,82 +12,87 @@ import TripList from './app/collections/trip_list';
 const tripList = new TripList();
 tripList.fetch();
 
-let bookTemplate;
+let tripTemplate;
 
-const render = function render(bookList) {
-  const $bookList = $('#book-list');
-  $bookList.empty();
-  bookList.forEach((book) => {
-    $bookList.append(bookTemplate(book.attributes));
+const render = function render(tripList) {
+  const $tripList = $('#trip-list');
+  $tripList.empty();
+  tripList.forEach((trip) => {
+    $tripList.append(tripTemplate(trip.attributes));
   });
 };
 
-const fields = ['title', 'author', 'publication_year']
+// const fields = ['name', 'continent', 'weeks']
 
-const events = {
-  addBook(event) {
-    event.preventDefault();
-    const bookData = {};
-    fields.forEach( (field) => {
-      bookData[field] = $(`input[name=${field}]`).val();
-    });
-    const book = new Book(bookData);
-    bookList.add(book);
-    book.save({}, {
-      success: events.successfulSave,
-      error: events.failedSave
-    });
-  },
-
-  sortBooks(event) {
-    console.log(event);
-    console.log(this);
-    const classes = $(this).attr('class').split(/\s+/);
-
-    bookList.comparator = classes[1];
-
-    if (classes.includes('current-sort-field')) {
-      $(this).removeClass('current-sort-field');
-      bookList.set(bookList.models.reverse());
-      render(bookList);
-
-    } else {
-      $('.current-sort-field').removeClass('current-sort-field');
-      $(this).addClass('current-sort-field');
-      bookList.sort();
-    };
-  },
-
-  successfulSave(book, response){
-    console.log('success!');
-    console.log(book);
-    console.log(response);
-    $('#status-messages ul').empty();
-    $('#status-messages ul').append(`<li>${book.get('title')} added!</li>`);
-    $('#status-messages ul').show();
-  },
-
-  failedSave(book, response) {
-    console.log('error');
-    console.log(book);
-    console.log(response);
-    book.destroy();
-    $('#status-messages ul').empty();
-    $('#status-messages ul').append(`<li>${book.get('title')} added!</li>`);
-    $('#status-messages ul').show();
-  },
-
-};
+// const events = {
+//   addBook(event) {
+//     event.preventDefault();
+//     const bookData = {};
+//     fields.forEach( (field) => {
+//       bookData[field] = $(`input[name=${field}]`).val();
+//     });
+//     const book = new Book(bookData);
+//     bookList.add(book);
+//     book.save({}, {
+//       success: events.successfulSave,
+//       error: events.failedSave
+//     });
+//   },
+//
+//   sortBooks(event) {
+//     console.log(event);
+//     console.log(this);
+//     const classes = $(this).attr('class').split(/\s+/);
+//
+//     bookList.comparator = classes[1];
+//
+//     if (classes.includes('current-sort-field')) {
+//       $(this).removeClass('current-sort-field');
+//       bookList.set(bookList.models.reverse());
+//       render(bookList);
+//
+//     } else {
+//       $('.current-sort-field').removeClass('current-sort-field');
+//       $(this).addClass('current-sort-field');
+//       bookList.sort();
+//     };
+//   },
+//
+//   successfulSave(book, response){
+//     console.log('success!');
+//     console.log(book);
+//     console.log(response);
+//     $('#status-messages ul').empty();
+//     $('#status-messages ul').append(`<li>${book.get('title')} added!</li>`);
+//     $('#status-messages ul').show();
+//   },
+//
+//   failedSave(book, response) {
+//     console.log('error');
+//     console.log(book);
+//     console.log(response);
+//     book.destroy();
+//     $('#status-messages ul').empty();
+//     $('#status-messages ul').append(`<li>${book.get('title')} added!</li>`);
+//     $('#status-messages ul').show();
+//   },
+//
+// };
 
 
 
 
 $(document).ready(() => {
-  bookTemplate = _.template($('#book-template').html());
-  $('#add-book-form').submit(events.addBook);
-  $('.sort').click(events.sortBooks);
+  tripTemplate = _.template($('#trip-template').html());
+//   $('.view-all-trips').on('click', function(){
+//   showAllTrips();
+// });
+$('.reservation-form').hide();
+console.log(tripList);
+  // $('#add-book-form').submit(events.addBook);
+  // $('.sort').click(events.sortBooks);
 
-  bookList.on('update', render, bookList);
-  bookList.on('sort', render, bookList);
+  // bookList.on('update', render, bookList);
+  // bookList.on('sort', render, bookList);
 
 });
