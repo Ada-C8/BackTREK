7b7d9b7e2009190c9b65318a1541a17bd9159c34
Shaunna <skwiens@gmail.com>
2017-11-28 15:13:02 -0800

    got trip list from api

--- src/app.js
+++ src/app.js
@@ -6,8 +6,33 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-console.log('it loaded!');
+//MODELS AND COLLECTIONS
+import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list';
+
+let tripTemplate;
+
+const tripList = new TripList();
+
+const render = function render(tripList) {
+  console.log(tripList);
+  const $trips = $('#trips')
+  $trips.empty();
+  tripList.forEach((trip) => {
+    $trips.append(tripTemplate(trip.attributes));
+    console.log(trip.attributes);
+    // console.log('trip loaded!');
+    // console.log(trip);
+  });
+}
+
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  tripTemplate = _.template($('#trip-template').html());
+
+
+  tripList.on('update', render, tripList);
+
+  tripList.fetch();
+
 });
