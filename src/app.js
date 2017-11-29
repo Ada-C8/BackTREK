8dff363f536f79d0e5ca7d1a607a8924617e69d5
Jessica Owens <vertige@gmail.com>
2017-11-28 18:11:43 -0800

    Added view for TripList functionality

--- src/app.js
+++ src/app.js
@@ -12,14 +12,43 @@ import Trip from './app/models/trip';
 // Backbone Collections
 import TripList from './app/collections/trip_list';
 
+const tripList = new TripList();
 
+let tripTemplate;
+
+// // Display List of Trips
+const displayList = function displayList() {
+  $('.home').removeClass('home');
+    $('#list').removeClass('hide');
+  //   $.get(apiUrl, (response) => {
+  //     let list = '<ul>';
+  //     response.forEach((thing) => {
+  //       list += `<li id="${thing.id}">${thing.name}</li>`;
+  //     });
+  //     list += '</ul>';
+  //     $('#list').html(list);
+  //   }).fail(() => {
+  //     $('#message').addClass('failure').html('Oops! Something went wrong!');
+  //   });
+};
+
+const render = function render(list) {
+  $('#trip-list').empty();
+  list.forEach((trip) => {
+    $('#trip-list').append(tripTemplate(trip.attributes));
+    // $('#trip-list').children('tr').attr('id', id);
+  });
+};
 
 $(document).ready( () => {
   // PREP
   $('#reservation-form').hide();
+  tripTemplate = _.template($('#trip-listing-template').html());
 
   // BASICS
-  // $('#get-list').click(displayList);
+  $('#get-list').click(displayList);
+  tripList.on('update', render, tripList);
+  tripList.fetch();
 });
 
 // $(document).ready(() => {
@@ -36,19 +65,3 @@ $(document).ready( () => {
 //     postReservation($(this));
 //   });
 // });
-
-// // Display List of Trips
-// const displayList = function displayList() {
-//   $('.home').removeClass('home');
-//   $('#list').removeClass('hide');
-//   $.get(apiUrl, (response) => {
-//     let list = '<ul>';
-//     response.forEach((thing) => {
-//       list += `<li id="${thing.id}">${thing.name}</li>`;
-//     });
-//     list += '</ul>';
-//     $('#list').html(list);
-//   }).fail(() => {
-//     $('#message').addClass('failure').html('Oops! Something went wrong!');
-//   });
-// };
