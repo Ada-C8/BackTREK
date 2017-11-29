27acb059a135499e0523f5e2c6bea805dbb9f198
Amy Lee <amy.yj.lee@gmail.com>
2017-11-28 16:16:42 -0800

    added showAllTrips method and fixed format of trip cost

--- src/app.js
+++ src/app.js
@@ -13,8 +13,63 @@ console.log('it loaded!');
 
 const tripList = new TripList();
 // let tripTemplate;
+
+const showAllTrips= () => {
+  const tripsTable = `<h2>All Trips</h2>
+
+    <table>
+      <thead>
+        <th class="sort id">ID</th>
+        <th class="sort name">name</th>
+        <th class="sort continent">Continent</th>
+        <th class="sort category">Category</th>
+        <th class="sort weeks">Weeks</th>
+        <th class="sort cost">Cost</th>
+      </thead>
+      <tbody id="trip-list">
+      </tbody>
+    </table>
+</section>`;
+  $('#all-trips').append(tripsTable);
+};
+
+const events = {
+  showAllTrips(event){
+    const tripsTable = `<h2>All Trips</h2>
+      <table>
+        <thead>
+          <th class="sort id">ID</th>
+          <th class="sort name">name</th>
+          <th class="sort continent">Continent</th>
+          <th class="sort category">Category</th>
+          <th class="sort weeks">Weeks</th>
+          <th class="sort cost">Cost</th>
+        </thead>
+        <tbody id="trip-list">
+        </tbody>
+      </table>`;
+    const table =
+    $('#all-trips').append(tripsTable);
+    render(tripList);
+  },
+
+}
+const render= function render(tripList) {
+  $('#trip-list').empty();
+  tripList.forEach((trip) => {
+    const tripCost= (trip.attributes.cost);
+    trip.set('cost', tripCost.toFixed(2));
+    $('#trip-list').append(tripTemplate(trip.attributes));
+  });
+};
+let tripTemplate;
 $(document).ready( () => {
-  const tripTemplate = _.template($('#trip-template').html());
-  $('#all-trips-btn').on('click', showAllTrips, tripList)
+  tripTemplate = _.template($('#trip-template').html());
+  tripList.fetch();
+  // $('#all-trips-btn').on('click', () => {
+  //   showAllTrips();
+  // });
+  $('#all-trips-btn').click(events.showAllTrips);
+  $('#all-trips').on('update', render);
 
 });
