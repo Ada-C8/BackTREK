1d886d6058eed8a88ae4a429ad53e2567e104020
Eva Sawyer <esawyer25@gmail.com>
2017-11-28 19:12:57 -0800

    all trips can be displayed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // Vendor Modules
 import $ from 'jquery';
 import _ from 'underscore';
+import Trip from 'app/models/trip.js'
+import TripList from 'app/collections/tripList.js'
 
 // CSS
 import './css/foundation.css';
@@ -8,6 +10,60 @@ import './css/style.css';
 
 console.log('it loaded!');
 
+
+
+// const render = function render(tripList) {
+//   tripList.forEach((trip) => {
+//     const tripTemplate = _.template($('#trip-template').html());
+//     const tripHTML = tripTemplate(trip.attributes);
+//     $('#trip-list').append(tripHTML);
+//   });
+// };
+
+const trip = new Trip({
+  id: 4,
+  name: 'Cairo to Zanzibar',
+  continent: 'Africa',
+  category: 'everything',
+  weeks: 5,
+  cost: 9599.99
+});
+
+// console.log(trip.attributes)
+// console.log(trip.get('id'))
+// console.log(trip.get('name'))
+
+const tripList = new TripList();
+tripList.fetch().done(function() {
+  console.log('done');
+  render(tripList);
+});
+// console.log(tripList);
+
+const render = function render(tripList) {
+  console.log(tripList);
+  tripList.forEach((trip) => {
+    console.log('here');
+    const tripTemplate =  _.template($('#trip-template').html());
+    const tripHTML = tripTemplate(trip.attributes);
+    console.log(tripHTML)
+    $('#trip-list').append(tripHTML);
+  });
+};
+
+
+
+
+// console.log(tripList.at[0])
+
+//
+// tripList.forEach((trip) => {
+//   console.log(`${ trip.get('name') }`);
+//   console.log('in here?')
+// });
+
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  // $('main').html('<h1>Hello World!</h1>');
+  // $('trip').append(trip);
+  render(tripList)
 });
---
+++ src/app/collections/tripList.js
@@ -0,0 +1,14 @@
+import Backbone from 'backbone';
+import Trip from '../models/trip';
+
+const TripList = Backbone.Collection.extend({
+  model: Trip,
+  url: 'https://ada-backtrek-api.herokuapp.com/trips',
+  // parse: function(response) {
+  //   console.log('this is the response')
+  //   console.log(response);
+    // return response['books'];
+  //  }
+});
+
+export default TripList;
