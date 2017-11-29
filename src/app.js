bba01e7eaf190220452421add8e00f804e381fa0
Shaunna <skwiens@gmail.com>
2017-11-28 22:24:24 -0800

    js clean-up

--- src/app.js
+++ src/app.js
@@ -10,47 +10,35 @@ import './css/style.css';
 import Trip from './app/models/trip';
 import TripList from './app/collections/trip_list';
 
+//selectors from the DOM
+const $tripsList = $('#trips-list')
+const $tripDescription = $('#trip-description')
+
+//templates
 let tripTemplate;
 let tripDetailsTemplate;
 
 const tripList = new TripList();
 
-const $tripsList = $('#trips-list')
-
-const $tripDescription = $('#trip-description')
-
+// render trips table
 const render = function render(tripList) {
+  tripDetailsTemplate = _.template($('#trip-details-template').html());
+
   $tripsList.empty();
   tripList.forEach((trip) => {
-    // console.log(trip);
-    // console.log(trip.attributes.category);
     $tripsList.append(tripTemplate(trip.attributes));
   });
 }
 
-// const renderTrip = function tripRender(trip) {
-//   console.log('rendering a change for at trip!')
-//   console.log(this);
-//   console.log(trip);
-//   $tripDescription.empty();
-//   $tripDescription.append(tripDetailsTemplate(trip.attributes));
-// }
-
 $(document).ready( () => {
   tripTemplate = _.template($('#trip-template').html());
 
-  tripDetailsTemplate = _.template($('#trip-details-template').html());
 
   // User Events
   $tripsList.on('click', 'tr', function getTrip() {
-    const tripID = $(this).attr('data-id');
+    const trip = new Trip({ id: $(this).attr('data-id') })
 
-    const trip = new Trip({ id: tripID })
-
-    console.log(`aaaa${tripID}`)
     trip.fetch().done(() => {
-      console.log(trip.attributes);
-      console.log(trip.attributes.category);
       $tripDescription.append(tripDetailsTemplate(trip.attributes));
     });
   });
@@ -58,7 +46,6 @@ $(document).ready( () => {
   // Data Events
   tripList.on('update', render, tripList);
 
-
+  // Implement when page loads
   tripList.fetch();
-
 });
