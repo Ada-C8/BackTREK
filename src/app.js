1fbdeb3ff923a8716af3711dbfa36b7671de2095
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-28 16:08:32 -0800

    Adds html and js for a successful underscore template. Got api trip data to load on page.

--- src/app.js
+++ src/app.js
@@ -8,23 +8,35 @@ import './css/style.css';
 
 // Our components
 import TripList from './app/collections/trip_list';
-// import Trip from './app/models/trip';
-
-
-let myTrip = {
-  "id":1,
-  "name":"Cairo to Zanzibar",
-  "continent":"Africa",
-  "category":"everything",
-  "weeks":5,
-  "cost":9599.99
-};
 
 const tripList = new TripList();
 
-tripList.add(myTrip);
-console.log(tripList);
+const TRIP_FIELDS = ["id", "name", "continent", "category", "weeks", "cost"];
+
+let tripTemplate;
+
+const render = function render(tripList) {
+  // iterate through the bookList, generate HTML
+  // for each model and attatch it to the DOM
+  const tripTableElement = $('#trip-list');
+
+  // clears the html so when we dynamically render again we get a new list vs just adding on
+  tripTableElement.html('');
+
+  tripList.forEach((trip) => {
+    const generatedHTML = tripTemplate(trip.attributes);
+    tripTableElement.append(generatedHTML);
+  });
+};
+
+
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  tripTemplate = _.template($('#trip-template').html());
+
+  // adding new models to a collection triggers an update event
+  tripList.on('update', render);
+
+  // get all of the trips from the API
+  tripList.fetch();
 });
--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -4,10 +4,10 @@ import Trip from '../models/trip';
 const Triplist = Backbone.Collection.extend({
   model: Trip,
   url: 'https://ada-backtrek-api.herokuapp.com/trips',
-  // comparator: 'title',
-  // parse: function(response) {
-  //   return response["books"];
-  // }
+  comparator: 'name',
+  parse: function(response){
+    return response;
+  }
 });
 
 export default Triplist;
