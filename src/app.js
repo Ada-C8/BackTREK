c05d06254dce4f05ca337acd72ec2f1b9e510531
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-11-28 17:00:04 -0800

    added trips column

--- src/app.js
+++ src/app.js
@@ -6,8 +6,33 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list'
+
 console.log('it loaded!');
 
+const tripList = new TripList();
+
+  console.log(tripList);
+
+let tripTemplate;
+
+const render = function render(tripList) {
+
+  const $tripList = $('#trip-list');
+  $tripList.empty();
+  tripList.forEach((trip) => {
+    $tripList.append(tripTemplate(trip.attributes));
+  });
+
+};
+
+
 $(document).ready( () => {
+  tripTemplate = _.template($('#trip-template').html());
   $('main').html('<h1>Hello World!</h1>');
+  
+  tripList.on('update', render, tripList);
+  tripList.fetch();
+
 });
