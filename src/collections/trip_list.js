27174a16351572b74a0c2906dc6a9b1783238c97
murog <guillermina.muro@gmail.com>
2017-11-28 17:41:19 -0800

    updated trip list to just return response

--- src/collections/trip_list.js
+++ src/collections/trip_list.js
@@ -3,8 +3,10 @@ import Trip from '../models/trip';
 
 const TripList = Backbone.Collection.extend({
   model: Trip,
-  url: 'https://ada-backtrek-api.herokuapp.com/trips',
+  url: 'http://ada-backtrek-api.herokuapp.com/trips',
   parse(response) {
     return response;
   }
 });
+
+export default TripList;
