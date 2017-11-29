4a7cd560f659f07c095d7847a1e9035b5a333ebf
Jessica Owens <vertige@gmail.com>
2017-11-28 23:18:09 -0800

    Finished display of single trip

--- src/app.js
+++ src/app.js
@@ -15,49 +15,50 @@ import TripList from './app/collections/trip_list';
 const tripList = new TripList();
 
 let tripTemplate;
-
-// // Display List of Trips
-const displayList = function displayList() {
-  $('.home').removeClass('home');
-    $('#list').removeClass('hide');
-  //   $.get(apiUrl, (response) => {
-  //     let list = '<ul>';
-  //     response.forEach((thing) => {
-  //       list += `<li id="${thing.id}">${thing.name}</li>`;
-  //     });
-  //     list += '</ul>';
-  //     $('#list').html(list);
-  //   }).fail(() => {
-  //     $('#message').addClass('failure').html('Oops! Something went wrong!');
-  //   });
-};
+let tripListTemplate;
 
 const render = function render(list) {
   $('#trip-list').empty();
   list.forEach((trip) => {
-    $('#trip-list').append(tripTemplate(trip.attributes));
-    // $('#trip-list').children('tr').attr('id', id);
+    $('#trip-list').append(tripListTemplate(trip.attributes));
+    $(`#${trip.attributes.id} td.category`).html(trip.capitalize(trip.attributes.category));
+    $(`#${trip.attributes.id} td.cost`).html(trip.attributes.cost.toFixed(2));
   });
 };
+
+const events = {
+  // Display Details of Trip
+  displayDetails() {
+    const id = $(this)[0].id;
+    const trip = tripList.get(id);
+    $('#list-wrapper').addClass('large-5');
+    $('#reservation-form').show();
+    $('#details').css('padding', '20px');
+    trip.fetch().done(() => {
+      $('#details').html(tripTemplate(trip));
+    });
+    $('#reservation-form').children('form').attr('id', id);
+    $('#price').html(`$${trip.attributes.cost.toFixed(2)} - Reserve!`);
+  }
+}
 
 $(document).ready( () => {
   // PREP
   $('#reservation-form').hide();
-  tripTemplate = _.template($('#trip-listing-template').html());
-
+  tripListTemplate = _.template($('#trip-list-template').html());
+  tripTemplate = _.template($('#trip-detail-template').html());
   // BASICS
-  $('#get-list').click(displayList);
+  $('#get-list').click(() => {
+    $('.home').removeClass('home');
+    $('#list').removeClass('hide');
+  });
+
+  $('#list').on('click', 'tr', events.displayDetails);
   tripList.on('update', render, tripList);
   tripList.fetch();
 });
 
 // $(document).ready(() => {
-//
-//   // BASICS
-//   $('#list').on('click', 'li', function fx() {
-//     $('#list-wrapper').addClass('large-5');
-//     displayDetails($(this)[0].id);
-//   });
 //
 //   // FORM
 //   $('form').submit(function fx(e) {
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -1,7 +1,15 @@
 import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
-
+  sPluralize(word, qty) {
+    if (qty === 1) {
+      return word;
+    }
+    return `${word}s`;
+  },
+  capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }
 });
 
 export default Trip;
