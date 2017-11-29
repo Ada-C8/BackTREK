e14e0cb3f944c593ce56c915f8a29d55eea7c292
averikitsch <aekitsch@gmail.com>
2017-11-28 18:59:08 -0800

    fetch working for single trip

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import TripList from './app/collections/trip_list';
 // Initialize
 const tripList = new TripList();
 const tripListTemplate = _.template($('#trip-list-template').html());
-// const tripTemplate = _.template($('#trip-template').html());
+const tripTemplate = _.template($('#trip-template').html());
 
 const render = function render(list) {
   const $list = $('#trip-list');
@@ -25,28 +25,78 @@ const events = {
   showTrips() {
     console.log('SHOW ME TRIPS!');
     $('button#show-trips').hide();
-
     $('#trip-table').show();
   },
   showTrip(event) {
+    $('#reservation').show();
     console.log('MA TRIP!');
     const trip = new Trip({id: event.target.parentElement.id});
-    const response = trip.fetch();
-    console.log(tripListTemplate(response));
+    trip.fetch({}).done(() => {
+      $('#show-trip').html(tripTemplate(trip.attributes));
+      console.log('YAY');
+    }).fail(() => {
+      console.log('OOPS');
+    });
+  },
+  addTrip(event) {
 
-    $('show-trip').html();
+  },
+  successfulSave(trip) {
+    $('#status-messages ul').empty();
+    $('#status-messages ul').append(`<li>You booked ${trip.name}!</li>`);
+    $('#status-messages').show();
+  },
+  failedSave(trip, response) {
+    $('#status-messages ul').empty();
+    Object.entries(response.responseJSON.errors).forEach((error) => {
+      for(let i = 0; i < error[1].length; i++) {
+        $('#status-messages ul').append(`<li>${error[0]}: ${error[1][i]}</li>`);
+      }
+    });
+
+    $('#status-messages').show();
+    trip.destroy();
   },
 }
 
 console.log('it loaded!');
 
 // -------------------------------------------------------
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// -------------------------------------------------------
+
 
 $(document).ready( function() {
+
   // On Start
   tripList.fetch();
   $('#sorting').hide();
   $('#trip-table').hide();
+  $('#reservation').hide();
 
   // Event Trigger
   tripList.on('update', render, tripList);
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -2,6 +2,10 @@ import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
   urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips',
+  // parse: function(response) {
+  //   console.log(response);
+  //   return response;
+  // }
 });
 
 export default Trip;
