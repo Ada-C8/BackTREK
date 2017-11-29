e05543c4bb2df21a4da03df082459d2ba1e4e4a0
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-11-29 14:07:08 -0800

    creating load details function

--- src/app.js
+++ src/app.js
@@ -13,26 +13,43 @@ console.log('it loaded!');
 
 const tripList = new TripList();
 
-  console.log(tripList);
-
 let tripTemplate;
 
 const render = function render(tripList) {
-
   const $tripList = $('#trip-list');
   $tripList.empty();
   tripList.forEach((trip) => {
     $tripList.append(tripTemplate(trip.attributes));
+    console.log(trip.attributes);
   });
 
 };
 
+const loadTrips = function loadTrips(){
+  tripList.on('update', render, tripList);
+  tripList.fetch();
+};
+let infoTemplate;
+
+const loadTripDetails = function loadTripDetails(tId) {
+  let trip = new Trip({id: tId}).fetch().done();
+  const $summary = $('#summary');
+  $summary.empty();
+  $summary.append(infoTemplate(trip.attributes));
+  console.log(trip);
+
+};
 
 $(document).ready( () => {
   tripTemplate = _.template($('#trip-template').html());
-  $('main').html('<h1>Hello World!</h1>');
-  
-  tripList.on('update', render, tripList);
-  tripList.fetch();
+  infoTemplate = _.template($('#info-template').html());
+  loadTrips();
+  $('.all-trips').on('click', function() {
+    $('table').toggleClass('active');
+  });
 
+  $('#trip-list').on('click', '.trip', function(){
+    let tripID = $(this).attr('data-id');
+    loadTripDetails(tripID);
+  });
 });
--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -4,9 +4,7 @@ import Trip from '../models/trip';
 const TripList = Backbone.Collection.extend({
   model: Trip,
   url: 'https://ada-backtrek-api.herokuapp.com/trips',
-  parse(response) {
-    return response;
-  }
+
 
 });
 
