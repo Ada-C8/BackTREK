c1ccf8ba940efd5e8425e379ea0ea6c76db230a2
IsabelDePapel <isuchanek@gmail.com>
2017-11-28 16:18:12 -0800

    get tripList and trip details using API

--- src/app.js
+++ src/app.js
@@ -6,8 +6,68 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-console.log('it loaded!');
+import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list';
+
+const tripList = new TripList();
+const trip = new Trip();
+
+let tripListTemplate;
+let tripTemplate;
+
+const getTrip = function getTrip(tripId) {
+
+};
+
+const render = function render(tripList) {
+  // empty existing list
+  const $tripList = $('#trip-list');
+  $tripList.empty();
+
+  tripList.forEach((trip) => {
+    $tripList.append(tripListTemplate(trip.attributes));
+  });
+  console.log(tripList);
+};
+
+const loadTrip = function loadTrip(trip) {
+  console.log('clicked');
+  const $tripDetail = $('#trip-detail');
+
+  $tripDetail.empty();
+  console.log(this);
+  console.log(typeof this);
+  // console.log(tripList);
+  // $tripDetail.append(tripTemplate(this));
+  // $tripDetail.append(tripTemplate(trip));
+};
+
+const events = {
+  getTrip(event) {
+    const tripId = event.currentTarget.id;
+    const url = `${tripList.url}/${tripId}`;
+    let trip = new Trip();
+    $.get(url, (response) => {
+      const $tripDetail = $('#trip-detail');
+
+      $tripDetail.empty();
+      $tripDetail.append(tripTemplate(response));
+    }).fail(function() {
+      console.log("failure");
+    });
+
+
+  }
+};
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  // load templates
+  tripListTemplate = _.template($('#trip-list-template').html());
+  tripTemplate = _.template($('#trip-template').html());
+
+  tripList.on('update', render, tripList);
+  $('#trip-list').on('click', 'tr', events.getTrip, this);
+
+
+  tripList.fetch();
 });
