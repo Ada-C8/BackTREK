5146b80405a9b726a708dbc74fbb36bdb2712c11
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-28 16:03:33 -0800

    Created trip template to render trip to view

--- src/app.js
+++ src/app.js
@@ -10,8 +10,29 @@ import './css/style.css';
 import Trip from './app/models/trip';
 import TripList from './app/collections/trip_list';
 
-console.log('it loaded!');
+const rawTripData = [
+  {
+    name: "Kinkakuji",
+    continent: "Asia",
+    category: "historical",
+    weeks: 1,
+    cost: 100
+  }
+];
+
+const tripList = new TripList(rawTripData);
+let tripTemplate;
+
+const render = function render(tripList) {
+  let $tripList = $('#trip-list');
+  $tripList.empty();
+  tripList.forEach((trip) => {
+    $tripList.append(tripTemplate(trip.attributes));
+  });
+};
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  tripTemplate = _.template($('#trip-template').html());
+  console.log(tripList);
+  render(tripList);
 });
