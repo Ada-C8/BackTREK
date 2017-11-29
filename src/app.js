0353fe5626a9f56fd5555b7a413505541bfbff4a
Gale Harrington <gale.harrington19@gmail.com>
2017-11-28 21:31:36 -0800

    Can show trip details when you click on a row

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import TripList from './app/collections/trip_list';
 
 // Vars that need to be defined up front
 let tripTemplate;
+let detailsTemplate;
 let trip = new Trip();
 const tripList = new TripList();
 
@@ -24,6 +25,12 @@ const render = function render(tripList) {
   });
 };
 
+const renderTripDetails = function renderTripDetails(trip) {
+  console.log('triggered renderTripDetails');
+  $('#trip-info').empty();
+  $('#trip-info').append(detailsTemplate(trip.attributes))
+};
+
 const getTrip = function getTrip() {
   const id = $(this).attr('id');
   trip = tripList.get(id);
@@ -34,6 +41,10 @@ const events = {
   successfulGetTrip(trip, response) {
     console.log('got it!')
     console.log(trip)
+    $('#trip-info').empty();
+    $('#trip-info').append(detailsTemplate(trip.attributes))
+    // trip.trigger('selected'); // trigger 'sync' eventHandler
+
   },
   failedGetTrip(trip, response) {
 
@@ -43,12 +54,15 @@ const events = {
 
 $(document).ready( () => {
   tripTemplate = _.template($('#trip-row-template').html());
+  detailsTemplate = _.template($('#trip-details-template').html());
 
   $('#explore-trips').on('click', function() {$('#trip-table').show()});
 
   tripList.on('update', render, tripList)
 
   tripList.fetch(); // will automatically call render
+
+  trip.on('selected', renderTripDetails, trip) // listening for 'selected'. will render tripdetails
 
   $('#trip-list').on('click', 'tr', getTrip)
 });
