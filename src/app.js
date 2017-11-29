4366f62b76c900055e30f445b8ef2c81d484acfa
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-28 16:10:32 -0800

    Add trip and trip-list classes

--- src/app.js
+++ src/app.js
@@ -6,8 +6,18 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+//CLASSES
+import TripList from './collections/trip_list';
+
 console.log('it loaded!');
 
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
-});
+  // $('main').html('<h1>Hello World!</h1>');
+
+
+
+
+
+
+
+}); //end of ready
---
+++ src/app/models/trip.js
@@ -0,0 +1,4 @@
+import Backbone from 'backbone';
+
+const Trip = Backbone.Model.extend({});
+export default Trip;
