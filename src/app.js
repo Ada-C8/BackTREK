cd1f76e94ed32168243a263dc562779fd15b25b5
Shaunna <skwiens@gmail.com>
2017-11-28 20:51:47 -0800

    get trip from api and trip details template

--- src/app.js
+++ src/app.js
@@ -11,26 +11,35 @@ import Trip from './app/models/trip';
 import TripList from './app/collections/trip_list';
 
 let tripTemplate;
+let tripDetailsTemplate;
 
 const tripList = new TripList();
 
+const $tripsList = $('#trips-list')
+
+const $tripDescription = $('#trip-description')
+
 const render = function render(tripList) {
-  console.log(tripList);
-  const $trips = $('#trips-list')
-  $trips.empty();
+  $tripsList.empty();
   tripList.forEach((trip) => {
-    $trips.append(tripTemplate(trip.attributes));
-    // console.log(trip.attributes);
-    // // console.log('trip loaded!');
-    // console.log(trip);
+    $tripsList.append(tripTemplate(trip.attributes));
   });
 }
 
 $(document).ready( () => {
   tripTemplate = _.template($('#trip-template').html());
 
+  tripDetailsTemplate = _.template($('#trip-details-template').html());
+
   // User Events
-  // ('').on('click', )
+  $tripsList.on('click', 'tr', function getTrip() {
+    console.log(this);
+    const tripID = $(this).attr('data-id');
+    console.log(tripID);
+    const trip = new Trip({ id: tripID}).fetch();
+    console.log(trip);
+    // $tripDescription.append(tripDetailsTemplate);
+  });
 
   // Data Events
   tripList.on('update', render, tripList);
