28d06078a8dcb92775c519ad1ec648a8c0025b71
averikitsch <aekitsch@gmail.com>
2017-11-28 16:04:02 -0800

    trip list

--- src/app.js
+++ src/app.js
@@ -5,9 +5,53 @@ import _ from 'underscore';
 // CSS
 import './css/foundation.css';
 import './css/style.css';
+import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list';
+
+// Initialize
+const tripList = new TripList();
+const tripListTemplate = _.template($('#trip-list-template').html());
+// const tripTemplate = _.template($('#trip-template').html());
+
+const render = function render(list) {
+  const $list = $('#trip-list');
+  // $list.empty();
+  list.forEach((trip) => {
+    $list.append(tripListTemplate(trip.toJSON()));
+  });
+}
+
+const events = {
+  showTrips() {
+    console.log('SHOW ME TRIPS!');
+    $('button#show-trips').hide();
+
+    $('#trip-table').show();
+  },
+  showTrip(event) {
+    console.log('MA TRIP!');
+    const trip = new Trip({id: event.target.parentElement.id});
+    const response = trip.fetch();
+    console.log(tripListTemplate(response));
+
+    $('show-trip').html();
+  },
+}
 
 console.log('it loaded!');
 
-$(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+// -------------------------------------------------------
+
+$(document).ready( function() {
+  // On Start
+  tripList.fetch();
+  $('#sorting').hide();
+  $('#trip-table').hide();
+
+  // Event Trigger
+  tripList.on('update', render, tripList);
+
+  // Event Handler
+  $('button#show-trips').click(events.showTrips);
+  $('#trip-list').click('tr', events.showTrip);
 });
