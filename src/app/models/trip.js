20cc153986d6b85be7979de987f0b13b60036f56
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-29 10:37:40 -0800

    add default values to trip model

--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -1,5 +1,14 @@
 import Backbone from 'backbone';
 
-const Trip = Backbone.Model.extend({});
+const Trip = Backbone.Model.extend({
+  defaults: {
+    name: 'Untitled Trip',
+    continent: 'Unknown',
+    category: 'none',
+    weeks: 0,
+    about: 'no info',
+    cost: 0,
+  }
+});
 
 export default Trip;
