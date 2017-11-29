eab9321705fec71a669300af90ffe8626d90e422
Angela Wilson <wilsonange@gmail.com>
2017-11-28 20:23:27 -0800

    display trips from api. still need to add .show() and .hide() functionality for trips

--- src/app.js
+++ src/app.js
@@ -6,10 +6,35 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-import './collections/trip_list'
+import TripList from './app/collections/trip_list'
+import Trip from './app/models/trip';
 
 console.log('it loaded!');
 
+const tripList = new TripList()
+
+let tripTemplate;
+
+const render = function render(tripList) {
+  const tripElement = $('#trip-list');
+  tripElement.html('');
+
+  tripList.forEach((trip) => {
+    const generatedHTML = tripTemplate(trip.attributes);
+    tripElement.append(generatedHTML)
+  });
+};
+
+console.log(tripList);
+
+// Jquery event handling
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+
+  tripTemplate = _.template($('#trip-template').html())
+
+  tripList.on('update', render);
+  tripList.fetch()
+  console.log(tripList);
+  // $('#load-trips').on('click', getTrips)
+
 });
