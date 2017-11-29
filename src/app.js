51720401d0c1dff8d8739ae689702bb442ba2d43
Angela Wilson <wilsonange@gmail.com>
2017-11-28 21:39:37 -0800

    hide all trips until trips button is clicked

--- src/app.js
+++ src/app.js
@@ -16,25 +16,36 @@ const tripList = new TripList()
 let tripTemplate;
 
 const render = function render(tripList) {
-  const tripElement = $('#trip-list');
-  tripElement.html('');
+  const $tripElement = $('#trip-list');
+  $tripElement.html('');
 
   tripList.forEach((trip) => {
     const generatedHTML = tripTemplate(trip.attributes);
-    tripElement.append(generatedHTML)
+    $tripElement.append(generatedHTML)
   });
+};
+
+const loadTrips = function loadTrips() {
+  tripList.fetch();
+  tripList.on('update', render, tripList);
 };
 
 console.log(tripList);
 
 // Jquery event handling
 $(document).ready( () => {
-
   tripTemplate = _.template($('#trip-template').html())
+  $('#trips').hide();
 
-  tripList.on('update', render);
+  // renders DOM when a new trip is added to the collection
+  tripList.on('update', render, tripList);
+
+  // gets all trips from API
   tripList.fetch()
   console.log(tripList);
-  // $('#load-trips').on('click', getTrips)
 
+  $('#load-trips').on('click', function (){
+    $('#trips').show();
+    loadTrips()
+  });
 });
