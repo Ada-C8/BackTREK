5751bb9d33ed8aab8e3cf60d4c276ef18bd763b6
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-28 16:54:12 -0800

    Successful upload of API information into table

--- src/app.js
+++ src/app.js
@@ -9,49 +9,29 @@ import './css/style.css';
 import TripsList from './app/collections/trips_list.js';
 import Trip from './app/models/trip.js';
 
+// variable declarations and functions
 
-const rawTripData = [
-  {
-    name: "Cairo to Zanzibar",
-    continent: "Africa",
-    about: 'So Awesome!',
-    weeks: 5,
-    cost: 9599.99
-  }, {
-    name: "Espana",
-    continent: "Europe",
-    about: "Motherland!",
-    weeks: 6,
-    cost: 'free'
-  }
-]
-
-const tripsList = new TripsList(rawTripData);
+const tripsList = new TripsList();
 
 const render = function render(tripsList) {
   const tripTableElement = $('#trips-list');
   tripsList.forEach((trip) => {
     const generatedHTML = tripsTemplate(trip.attributes);
-    console.log(generatedHTML);
     tripTableElement.append(generatedHTML);
   })
 };
 
-
-
-// tripsList.fetch();
-
+let tripsTemplate;
 
 // let tripTemplate;
 
-let tripsTemplate;
-
 
 $(document).ready( () => {
-  tripsTemplate = _.template($('#trips-template').html());
-
+  tripsTemplate = _.template($('#trips-list-template').html());
   // tripTemplate = _.template($('#trip-template').html());
 
-  render(tripsList);
+  tripsList.on('update', render);
+
+  tripsList.fetch();
 
 });
