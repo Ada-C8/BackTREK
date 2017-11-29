1a5307e850f108383d1a6a0f3cf2d77355e2b5cb
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-28 16:39:55 -0800

    Add showtrips function

--- src/app.js
+++ src/app.js
@@ -6,13 +6,37 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
-//CLASSES
-import TripList from './collections/trip_list';
-
+// CLASSES
+import TripList from 'app/collections/trip_list';
 console.log('it loaded!');
 
+// array of fileds for sorting
+const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'cost'];
+// create variable for using in document.ready
+const tripList = new TripList();
+// create variable and set it in doucment.ready
+let tripTemplate;
+
+const showTrips = function(tripList) {
+  const tripTableElement = $('#trip-list');
+  tripTableElement.html('');
+  // console.log(tripList);
+  for (let trip of tripList.models) {
+    const generatedHTML = tripTemplate(trip.attributes);
+    // console.log(trip);
+    tripTableElement.append(generatedHTML);
+  }
+}
+
 $(document).ready( () => {
+  tripTemplate = _.template($('#trip-template').html());
   // $('main').html('<h1>Hello World!</h1>');
+
+  tripList.on('update', showTrips);
+  tripList.on('sort', showTrips);
+
+  tripList.fetch();
+
 
 
 
