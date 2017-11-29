76cb4132b786f65641b1e81bbdfd6c952c44e32d
IsabelDePapel <isuchanek@gmail.com>
2017-11-29 15:21:12 -0800

    add trip validations to trip model, add check for is valid when adding trip

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Vendor Modules
 import $ from 'jquery';
 import _ from 'underscore';
+import 'jquery-modal';
 
 // CSS
 import './css/foundation.css';
@@ -9,19 +10,14 @@ import './css/style.css';
 import Trip from './app/models/trip';
 import TripList from './app/collections/trip_list';
 
-// let modal = require('jquery-modal')($);
-
 const tripList = new TripList();
-const trip = new Trip();
+// const trip = new Trip();
 
 let tripListTemplate;
 let tripTemplate;
 
-const addTripForm = function addTripForm() {
-
-};
-
-const render = function render(tripList) {
+// renders
+const renderTripList = function renderTripList(tripList) {
   // empty existing list
   const $tripList = $('#trip-list');
   $tripList.empty();
@@ -32,47 +28,112 @@ const render = function render(tripList) {
   console.log(tripList);
 };
 
-const events = {
-  getTrip(event) {
-    const tripId = event.currentTarget.id;
-    const url = `${tripList.url}/${tripId}`;
+const renderTrip = function renderTrip(trip) {
+  const $tripDetail = $('#trip-detail');
 
-    // let trip = new Trip({id: tripId});
-
-    let trip = new Trip({id: tripId});
-    console.log(trip.fetch());
-    trip.fetch({
-      success: function(response) {
-        console.log(response.attributes);
-
-        const $tripDetail = $('#trip-detail');
-
-        $tripDetail.empty();
-        $tripDetail.append(tripTemplate(response.attributes));
-      },
-      // how is this handled???
-      error: function(response) {
-        console.log(response.attributes);
-      }
-    });
-  },
-
-  addTrip() {
-    
-  }
-
+  $tripDetail.empty();
+  $tripDetail.append(tripTemplate(trip.attributes));
 };
+
+const cancelSubmit = function cancelSubmit() {
+  $(this).parent().trigger('reset');
+  $('.error-messages').empty();
+};
+
+const readTripForm = function readTripForm() {
+  const tripFields = ['name', 'category', 'continent', 'weeks', 'about', 'cost'];
+
+  const tripData = {};
+
+  tripFields.forEach((field) => {
+    tripData[field] = $(`#add-trip-form [name$=${field}]`).val();
+  });
+
+  return tripData;
+};
+
+const getTrip = function getTrip(event) {
+  const tripId = event.currentTarget.id;
+  let trip = new Trip({id: tripId});
+
+  trip.fetch({
+    success: function(trip, response, options) {
+
+      // render error if no content
+      if (options.xhr.status === 204) {
+        const html = '<h3>Trip Not Found</h3>';
+        $('.detail.status-messages').append(html);
+      } else {
+        renderTrip(trip);
+      }
+    },
+    // how is this handled???
+    error: function(trip) {
+      console.log(trip);
+    }
+  });
+};
+
+const successfulSave = function successfulSave(trip) {
+  $('.error-messages').empty();
+
+  tripList.add(trip);
+  $.modal.close();
+};
+
+const failedSave = function failedSave(trip, response) {
+  const $errors = $('.error-messages');
+
+  $errors.empty();
+  $errors.append('<h4>Failed to save<h4><ul>');
+
+  const errorList = response.responseJSON.errors;
+
+  Object.keys(errorList).forEach((errorType) => {
+    errorList[errorType].forEach((error) => {
+      $errors.append(`<li>${errorType}: ${error}</li>`);
+    });
+  });
+  $errors.append('</ul>');
+};
+
+const addTrip = function addTrip(event) {
+  event.preventDefault();
+
+  const trip = new Trip(readTripForm());
+
+  // client side validations
+  if (trip.isValid()) {
+    trip.save({}, {
+      success: successfulSave,
+      error: failedSave
+    });
+  } else {
+    console.log(trip.validationError);
+  }
+  // trip.save({}, {
+  //   success: successfulSave,
+  //   error: failedSave
+  // });
+};
+
+const reserveTrip = function reserveTrip(event) {
+  event.preventDefault();
+  
+};
+
 
 $(document).ready( () => {
   // load templates
   tripListTemplate = _.template($('#trip-list-template').html());
   tripTemplate = _.template($('#trip-template').html());
 
-  tripList.on('update', render, tripList);
-  $('#trip-list').on('click focus', 'tr', events.getTrip, this);
+  tripList.on('update', renderTripList, tripList);
+  $('#trip-list').on('click focus', 'tr', getTrip, this);
 
-  // $('#add-trip').click(events.addTrip);
 
+  $('.cancel').on('click', cancelSubmit);
+  $('#add-trip').on('click', addTrip);
 
   tripList.fetch();
 });
