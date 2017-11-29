85f54f082a618198b26cdcf4ca91b57bea989778
Amy Cash <amymcash@gmail.com>
2017-11-28 17:53:30 -0800

    Add trip model and import to js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,33 @@ import './css/style.css';
 
 console.log('it loaded!');
 
+// models and collections
+import Trip from './app/models/trip';
+
+// practice data
+
+// [{name :"Cairo to Zanzibar","continent":"Africa","category":"everything","weeks":5,"cost":9599.99},{"id":2,"name":"Everest Base Camp Trek","continent":"Asia","category":"adventure","weeks":2,"cost":967.5},{"id":3,"name":"Golden Triangle","continent":"Asia","category":"historical","weeks":1,"cost":590.53},
+
+const testTrip = new Trip({
+    name: "Cairo",
+    continent: "Africa",
+    category: "everything",
+    weeks: 5,
+    cost: 9599.99
+  });
+
+  // console.log(testTrip.cid);
+  console.log(testTrip.attributes);
+
+
+
+
+
+
+
+
+
+
 $(document).ready( () => {
   $('main').html('<h1>Hello World!</h1>');
 });
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -3,7 +3,7 @@
 import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
-
+  // create a Trip subclass that inherits from Backbone.Model 
 });
 
 export default Trip;
