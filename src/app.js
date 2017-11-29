b31f49b3acc385333fc47329b9fbf170458d475a
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-29 12:47:02 -0800

    add initial show logic

--- src/app.js
+++ src/app.js
@@ -11,21 +11,36 @@ import Trip from './app/models/trip';
 import TripList from './app/collections/trip-list';
 
 let tripTemplate;
+let tripDetailTemplate;
 
 const tripList = new TripList();
 
 const render = function render(tripList) {
   const tripListElement = $('#trip-list tbody');
   tripList.forEach((trip) => {
-    const generatedHTML = tripTemplate(trip.attributes);
+    const generatedHTML = $(tripTemplate(trip.attributes));
+    generatedHTML.on('click', show);
     tripListElement.append(generatedHTML);
   });
 };
 
+const show = function show(e) {
+  const trip = $(e.target).closest('tr');
+  const id = trip[0].id;
+  const generatedHTML = tripDetailTemplate(trip);
+  trip.html(generatedHTML);
+};
+
 $(document).ready( () => {
+  $('#trip-list').hide();
   tripTemplate = _.template($('#trip-template').html());
-  tripList.fetch();
-  render(tripList);
+  tripDetailTemplate = _.template($('#trip-detail-template').html());
 
   tripList.on('update', render);
+
+  $('#intro-button').on('click', (e) => {
+    $('#intro-button').hide();
+    tripList.fetch();
+    $('#trip-list').show();
+  });
 });
