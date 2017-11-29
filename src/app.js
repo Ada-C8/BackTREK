133f20bf1667ebd4131578848738c39d9a58ca7d
Jan Edrozo <jedrozo@gmail.com>
2017-11-28 16:59:54 -0800

    W1:A user can click a link to see a list of trips requirement complete

--- src/app.js
+++ src/app.js
@@ -6,8 +6,47 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+// MODELS
+import Trip from './app/models/trip';
+
+// COLLECTION
+import TripList from './app/collections/trip_list';
+
+// INITITAL SET UP CHECK
 console.log('it loaded!');
 
+const tripList = new TripList();
+// console.log('TRIPLIST:');
+// console.log(tripList);
+
+let tripTemplate;
+
+const render = function render(tripList) {
+  console.log('TOP OF RENDER')
+  console.log(tripList);
+  const $tripList = $('#trip-list');
+  //Clears the list so when you re-render it doesn't print duplicates
+  $tripList.empty();
+
+  tripList.forEach((trip) => {
+    $tripList.append(tripTemplate(trip.attributes));
+    console.log(trip.attributes);
+  });
+};
+
+const loadTrips = function loadTrips() {
+  tripList.fetch();
+  tripList.on('update', render, tripList);
+};
+
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  $('#all-trips-table').hide();
+  tripTemplate = _.template($('#trip-template').html());
+
+  // EVENTS
+  // To view All Trips
+  $('#all-trips').on('click', function() {
+    $('#all-trips-table').show();
+    loadTrips();
+  });
 });
---
+++ src/app/models/trip.js
@@ -0,0 +1,10 @@
+import Backbone from 'backbone';
+
+// 1. Define Model and give it a name
+const Trip = Backbone.Model.extend({
+  // custom code; we are going to use Book as a class; so we can get an instance of Model Book
+});
+
+// Makes sure when book.js is imported, they get Book automatically
+// You can export different things in a file, but it is best practice to export 1 thing per file.
+export default Trip;
