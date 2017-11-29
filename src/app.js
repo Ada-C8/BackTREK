fa3a3e505a126c10da47f258e7278fdf3400ed54
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-29 13:08:26 -0800

    Add templates for back button and table head

--- src/app.js
+++ src/app.js
@@ -8,34 +8,62 @@ import './css/style.css';
 
 // CLASSES
 import TripList from 'app/collections/trip_list';
+
 console.log('it loaded!');
 
-// array of fileds for sorting
+// array of fields for sorting
 const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'cost'];
 // create variable for using in document.ready
 const tripList = new TripList();
-// create variable and set it in doucment.ready
-let tripTemplate;
+// create variable and set it in document.ready
 
-const showTrips = function(tripList) {
+//define templates
+let tripTemplate;
+let backTemplate;
+let headTemplate;
+
+const getTrips = function(tripList) {
+  let back = backTemplate();
+  let tableHead = headTemplate();
+
   const tripTableElement = $('#trip-list');
   tripTableElement.html('');
-  // console.log(tripList);
   for (let trip of tripList.models) {
     const generatedHTML = tripTemplate(trip.attributes);
-    // console.log(trip);
     tripTableElement.append(generatedHTML);
   }
-}
+
+  // $('.trips thead').html(tableHead);
+  // $('.back').html(back);
+  // $('#trips').toggle();
+} // end of getTrips
+
+const getTrip = function() {
+
+} // end of getTrip
+
+
 
 $(document).ready( () => {
+  //templates
   tripTemplate = _.template($('#trip-template').html());
-  // $('main').html('<h1>Hello World!</h1>');
+  backTemplate = _.template($('#back-button').html());
+  headTemplate = _.template($('#tripsHead').html());
 
-  tripList.on('update', showTrips);
-  tripList.on('sort', showTrips);
+  //get all trips on click
+  $('.trips').on('click', '#trips', function() {
+    tripList.on('update', getTrips);
+    tripList.on('sort', getTrips);
+    tripList.fetch();
+  })
 
-  tripList.fetch();
+  //go back from trips
+  $('body').on('click', '.back, #title', function() {
+    $('.trips table').html('');
+    $('.back').html('');
+    // $('#trips').toggle();
+  })
+
 
 
 
