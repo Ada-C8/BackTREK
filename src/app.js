d78d19cc9040d3bf19ab2af740e1fee366e7aa49
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-28 23:01:28 -0800

    Successful new trip form with submission - need to work on error handling

--- src/app.js
+++ src/app.js
@@ -10,28 +10,128 @@ import TripsList from './app/collections/trips_list.js';
 import Trip from './app/models/trip.js';
 
 // variable declarations and functions
+const TRIP_FIELDS = ['name', 'continent', 'about', 'category', 'weeks', 'cost'];
+
+const RES_FIELDS = ['name', 'age', 'email'];
+
+let tripsTemplate;
+
+let tripTemplate;
 
 const tripsList = new TripsList();
 
-const render = function render(tripsList) {
+
+// render all trips
+const renderTrips = function renderTrips(list) {
   const tripTableElement = $('#trips-list');
-  tripsList.forEach((trip) => {
+  tripTableElement.html('');
+
+  list.forEach((trip) => {
     const generatedHTML = tripsTemplate(trip.attributes);
     tripTableElement.append(generatedHTML);
   })
 };
 
-let tripsTemplate;
+// render individual trip
+const renderTrip = function renderTrip(trip) {
+  const tripElement = $('#trip-detail');
 
-// let tripTemplate;
+  const generatedHTML = tripTemplate(trip.attributes);
+  tripElement.html(generatedHTML);
+};
+
+// add trip handler
+const addTripHandler = function(event) {
+  event.preventDefault();
+
+  console.log('in the add trip handler');
+  const tripData = {};
+  TRIP_FIELDS.forEach((field) => {
+    // select the input corresponding to the field we want
+    const inputElement = $(`#add-trip-form input[name="${ field }"]`);
+    const value = inputElement.val();
+    tripData[field] = value;
+
+    inputElement.val('');
+  });
+
+  console.log("Read trip data");
+  console.log(tripData);
+
+  const trip = new Trip(tripData);
+
+  trip.save({}, {
+    success: (model, response) => {
+      tripsList.add(model);
+      reportStatus('success', 'Successfully add a trip!');
+    },
+    error: (model, response) => {
+      console.log('Failed to save a trip! Server response:');
+      console.log(response);
+
+      const errors = response.responseJSON["errors"];
+      for (let field in errors) {
+        for (let problem of errors[field]) {
+          reportStatus('error',`${field}: ${problem}`);
+        }
+      }
+    },
+  });
+};
 
 
-$(document).ready( () => {
+///NOTE //// Bug to fix
+// 
+// app.js:66 Uncaught ReferenceError: reportStatus is not defined
+//     at success (app.js:66)
+//     at Object.options.success (backbone.js:638)
+//     at fire (jquery.js:3317)
+//     at Object.fireWith [as resolveWith] (jquery.js:3447)
+//     at done (jquery.js:9272)
+//     at XMLHttpRequest.<anonymous> (jquery.js:9514)
+
+$(document).ready(() => {
+  //underscore
   tripsTemplate = _.template($('#trips-list-template').html());
-  // tripTemplate = _.template($('#trip-template').html());
+  tripTemplate = _.template($('#trip-template').html());
 
-  tripsList.on('update', render);
-
+  //backbone
+  tripsList.on('update', renderTrips);
   tripsList.fetch();
+
+  // jquery
+  $('#trips-list').on('click', '.trips', function(event) {
+    const trip = new Trip({ id: $(this).data("id")});
+
+    trip.on('change', renderTrip);
+    trip.fetch();
+  });
+
+  // user can create a new trip
+
+  // $('#add-trip-button').on('click', function(event) {
+  //   const biancasTrip = {
+  //     name: "Bianca's trip to Spain",
+  //     continent: "Europe",
+  //     about: "Enjoy tapas!",
+  //     category: "Culture",
+  //     weeks: 6,
+  //     cost: 2300
+  //   };
+  //
+  //   const trip = new Trip(biancasTrip);
+  //
+  //   trip.save({}, {
+  //     success: (model, response) => {
+  //       tripsList.add(model);
+  //     }
+  //   });
+  // });
+
+
+  $('#add-trip-form').on('submit', addTripHandler);
+
+  //reserve a spot on a trip
+
 
 });
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -1,7 +1,7 @@
 import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
-
+  urlRoot: 'https://trektravel.herokuapp.com/trips/',
 });
 
 export default Trip;
