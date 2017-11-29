ae27560feef7de0fe2ab1bdd0c382477c8cb2a65
Amy Cash <amymcash@gmail.com>
2017-11-28 20:42:50 -0800

    Successfully get all trips

--- src/app.js
+++ src/app.js
@@ -11,33 +11,12 @@ console.log('it loaded!');
 // models and collections
 import Trip from './app/models/trip';
 import TripList from './app/collections/trip_list';
-// practice data
 
-const testTrip = new Trip({
-    name: "Cairo",
-    continent: "Africa",
-    category: "everything",
-    weeks: 5,
-    cost: 9599.99
-  });
-
-const testTripTwo = new Trip({
-  name: "Place",
-  continent: "Asia",
-  category: "Fun",
-  weeks: 2,
-  cost: 1000.15
-})
-
-//
-
-const tripList = new TripList();
-tripList.add(testTrip);
-tripList.add(testTripTwo);
-
-console.log(tripList.at(0));
+const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'cost'];
 
 let tripTemplate;
+
+
 
 const render = function render(tripList) {
   const tripListElement = $('#trip-list');
@@ -56,10 +35,18 @@ $(document).ready( () => {
   console.log('in document ready');
   tripTemplate = _.template($('#trip-template').html() ); //same for collections and models
 
+  // Builds a collection
+  const tripList = new TripList();
+
+  // gets date from APi and triggers an Update Event
+  tripList.fetch();
+
+  tripList.on('update', render);
+  //so the fetch will trigged update and then render will render the data into the template.
 
   // const tripList = new TripList();
-  render(tripList);
-  // The render function takes the place of the final part of the function for a single item.
+  // render(tripList);
+  // The render function takes the place of the final part of the function for a single item. i.e. the part that goes through each item and turns it into html
 
 
 
@@ -74,6 +61,31 @@ $(document).ready( () => {
   // console.log(tripHTML);
   // $('#trip-list').append(tripHTML);
   //
+  // practice data
+
+  // const testTrip = new Trip({
+  //     name: "Cairo",
+  //     continent: "Africa",
+  //     category: "everything",
+  //     weeks: 5,
+  //     cost: 9599.99
+  //   });
+  //
+  // const testTripTwo = new Trip({
+  //   name: "Place",
+  //   continent: "Asia",
+  //   category: "Fun",
+  //   weeks: 2,
+  //   cost: 1000.15
+  // })
 
 
+
+  //
+
+  // const tripList = new TripList();
+  // tripList.add(testTrip);
+  // tripList.add(testTripTwo);
+  //
+  // console.log(tripList.at(0));
 //
