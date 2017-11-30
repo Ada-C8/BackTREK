ce16aef9e424f900426a7f11229717c316a0187f
Shaunna <skwiens@gmail.com>
2017-11-30 14:39:39 -0800

    reservation model added

---
+++ src/app/models/reservation.js
@@ -0,0 +1,15 @@
+import Backbone from 'backbone';
+
+const Reservation = Backbone.Model.extend({
+  // url: function() {
+  //   return 'https://ada-backtrek-api.herokuapp.com/trips/' + this.get('tripID') + '/reservations'
+  // },
+  // initialize: function(attributes) {
+  //   console.log(attributes);
+  // }
+  // validate: function(attributes) {
+  //   console.log(attriby)
+  // }
+});
+
+export default Reservation
