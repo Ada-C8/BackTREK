9f8ab57865bfa2a3f3f35a2a60a3fcf8afeaa69d
averikitsch <aekitsch@gmail.com>
2017-11-30 15:27:41 -0800

    filtering added

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ import Reservation from './app/models/reservation';
 
 // Initialize
 const tripList = new TripList();
+let filteredTrips;
 const tripListTemplate = _.template($('#trip-list-template').html());
 const tripTemplate = _.template($('#trip-template').html());
 
 const render = function render(list) {
   const $list = $('#trip-list');
-  // $list.empty();
+  $list.empty();
   list.forEach((trip) => {
     $list.append(tripListTemplate(trip.toJSON()));
   });
@@ -29,18 +30,22 @@ const events = {
   showTrips() {
     console.log('SHOW ME TRIPS!');
     $('button#show-trips').hide();
+    $('#filter').show();
     $('#trip-table').show();
   },
   showTrip(event) {
+    console.log(event.target.parentElement);
+    $('.selected-trip').removeClass('selected-trip');
+    $(event.target.parentElement).addClass('selected-trip');
     $('#reservation').show();
     const trip = new Trip({id: event.target.parentElement.id});
     trip.fetch({}).done(() => {
-      $('#show-trip').html(tripTemplate(trip.attributes));
       console.log('YAY');
+      $('#show-trip').html(tripTemplate(trip.attributes));
       $('form').attr('action', `${trip.url()}/reservations`);
     }).fail(() => {
-      $('#show-trip').html('<p>Looks like that trip left without you...</p>');
       console.log('OOPS');
+      $('#show-trip').html('<p>Looks like that trip left without you...</p>');
     });
   },
   addTrip(event) {
@@ -53,20 +58,20 @@ const events = {
     });
     const trip = new Trip(tripData);
     if (trip.isValid()) {
-      // tripList.add(trip);
       trip.save({}, {
         success: events.successfulSave,
         error: events.failedSave,
       });
     } else {
       Object.entries(trip.validationError).forEach((error) => {
-          $(`label[for=${error[0]}] span`).remove();
-          $(`label[for=${error[0]}]`).append(`<span class="error">: ${error[1]}</span>`);
-          $(`input[name=${error[0]}]`).addClass('error');
+        $(`label[for=${error[0]}] span`).remove();
+        $(`label[for=${error[0]}]`).append(`<span class="error">: ${error[1]}</span>`);
+        $(`input[name=${error[0]}]`).addClass('error');
       });
     }
   },
   successfulSave(trip) {
+    tripList.fetch();
     $('.modal').css('display','none');
     $('#status-messages ul').empty();
     $('#status-messages ul').append(`<li>You added ${trip.get('name')}!</li>`);
@@ -92,7 +97,6 @@ const events = {
     });
     const r = new Reservation(reservationData);
     if (r.isValid()) {
-      // tripList.add(trip);
       r.save({}, {
         url: $('form').attr('action'),
         success: events.successfulBook,
@@ -100,9 +104,9 @@ const events = {
       });
     } else {
       Object.entries(r.validationError).forEach((error) => {
-          $(`label[for=${error[0]}] span`).remove();
-          $(`label[for=${error[0]}]`).append(`<span class="error">: ${error[1]}</span>`);
-          $(`input[name=${error[0]}]`).addClass('error');
+        $(`label[for=${error[0]}] span`).remove();
+        $(`label[for=${error[0]}]`).append(`<span class="error">: ${error[1]}</span>`);
+        $(`input[name=${error[0]}]`).addClass('error');
       });
     }
   },
@@ -114,10 +118,61 @@ const events = {
   failedBook(reservation, response) {
     $('#status-messages ul').empty();
     Object.entries(response.responseJSON.errors).forEach((error) => {
-        $('#status-messages ul').append(`<li>${error[0]}: ${error[1]}</li>`);
+      $('#status-messages ul').append(`<li>${error[0]}: ${error[1]}</li>`);
     });
     $('#status-messages').show();
     reservation.destroy();
+  },
+  sortTrips() {
+    console.log('you clicked a sort');
+    // get class
+    const classes = $(this).attr('class').split(/\s+/);
+    // css styling
+    $('.sort').removeClass('current-sort-field');
+    $(this).addClass('current-sort-field');
+    console.log(filteredTrips);
+    if (filteredTrips) {
+      if (tripList.comparator === classes[1]) {
+        filteredTrips.reverse();
+      } else {
+        console.log(classes[1]);
+        filteredTrips.sort(function(a, b) {
+          const textA = a.get(classes[1]);
+          const textB = b.get(classes[1]);
+          return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        });
+      }
+      // console.log(filteredTrips);
+      tripList.trigger('sort', filteredTrips);
+      tripList.comparator = classes[1];
+    } else {
+      if (tripList.comparator === classes[1]) {
+        tripList.models.reverse();
+        tripList.trigger('sort', tripList);
+      } else {
+        tripList.comparator = classes[1];
+        tripList.sort();
+      }
+    }
+  },
+  filterTrips() {
+    const category = $('#filter-category').val();
+    const input = $('#filter-form input').val();
+    const typing = input ? input.toLowerCase() : input;
+    if (category && typing) {
+      console.log('filter me!');
+      const filter = _.filter(tripList.models, function(trip) {
+        if (['name', 'continent', 'category'].includes(category)) {
+          const tripName = trip.get(category).toLowerCase();
+          return tripName.includes(typing);
+        } else {
+          const tripName = trip.get(category);
+          return tripName <= typing;
+        }
+      });
+      tripList.trigger('sort', filter);
+      filteredTrips = filter;
+    }
   },
 }
 
@@ -125,7 +180,7 @@ console.log('it loaded!');
 
 // -------------------------------------------------------
 // Get the modal
-let $modal = $('.modal');
+const $modal = $('.modal');
 
 $('#add-trip').on('click', function() {
   $('.modal').css('display', 'block');
@@ -149,21 +204,26 @@ $(document).ready( function() {
 
   // On Start
   tripList.fetch();
-  //   {}, {
-  //   error: () => {
-  //     $('#trip-table').html('<p>All the trips are on vacation</p>')
-  //   }
-  // });
-  $('#sorting').hide();
+
+  $('#filter').hide();
   $('#trip-table').hide();
   $('#reservation').hide();
 
   // Event Trigger
-  tripList.on('update', render, tripList);
   $('#add-trip-form').submit(events.addTrip);
   $('#reservation-form').submit(events.addReservation);
-
-  // Event Handler
+  $('.sort').click(events.sortTrips);
   $('button#show-trips').click(events.showTrips);
   $('#trip-list').click('tr', events.showTrip);
+  $('#filter-form input').keyup(events.filterTrips);
+  $('select#filter-category').change(events.filterTrips);
+
+  $('#filter-form input').keypress(function(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  });
+
+  tripList.on('update', render, tripList);
+  tripList.on('sort', render, tripList);
 });
