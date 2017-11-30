2c216b59196303295ba9382a6235451767936352
Maria McGrew <mcgmar1@outlook.com>
2017-11-29 22:58:23 -0800

    loading all trips on .click event

--- src/app.js
+++ src/app.js
@@ -15,22 +15,50 @@ const tripList = new TripList();
 
 let tripTemplate;
 
-const render = function render(tripList) {
-  const tripListElement = $('#trip-list');
-  tripListElement.empty();
+// const allTrips = function allTrips(tripList) {
+//   const tripListElement = $('#trip-list');
+//   tripListElement.empty();
+//
+//   tripList.forEach((trip) => {
+//     console.log(`Rendering trip ${trip.get('name')}`);
+//     let tripHTML = tripTemplate(trip.attributes);
+//     tripListElement.append($(tripHTML));
+//   });
+// };
 
-  tripList.forEach((trip) => {
-    // console.log(`Rendering trip ${trip.get('name')}`);
-    let tripHTML = tripTemplate(trip.attributes);
-    tripListElement.append($(tripHTML));
-  });
+// const loadTrip = function loadTrip(id) {
+//   $.get(`https://ada-backtrek-api.herokuapp.com/trips/${id}`,
+//     (response) => {
+//       const tripInfo =
+//         `<div>
+//           <p> Trip name: ${response.name}</p>
+//           <p> Category: ${response.category}</p>
+//           <p> Continent: ${response.continent}</p>
+//           <p> Description: ${response.about}</p>
+//           <p> Duration: ${response.weeks} weeks</p>
+//           <p> Cost: ${response.cost}</p>
+//         </div>`;
+//       }
+//     )
+// };
+
+const events = {
+
+  allTrips(event) {
+    const tripListElement = $('#trip-list');
+    tripListElement.empty();
+    $('h2').text('Trip Options');
+    $('#load_trips').show();
+    tripList.forEach((trip) => {
+      console.log(`Rendering trip ${trip.get('name')}`);
+      let tripHTML = tripTemplate(trip.attributes);
+      tripListElement.append($(tripHTML));
+    });
+  }
 };
-
 
 $(document).ready( () => {
   tripTemplate = _.template($('#trip-template').html());
-
-  tripList.on('sort', render, tripList);
   tripList.fetch();
-
+  $('#trips_button').click(events.allTrips);
 });
