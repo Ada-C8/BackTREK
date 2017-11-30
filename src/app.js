3a20ec5f4c76aed2d6c4f6840eb1938174dc096a
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-29 17:06:48 -0800

    added model validations for trip

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import './css/style.css';
 
 // Our components
 import TripList from './app/collections/trip_list';
+import Trip from './app/models/trip';
 
 const tripList = new TripList();
 
@@ -18,18 +19,27 @@ let detailsTemplate;
 
 const renderDetails = function renderDetails(trip){
   const detailsElement = $('#trip-details');
+  detailsElement.html('');
 
-  // clears between clicks
-  // api call is really slow
-  trip.fetch({
-    success: (model) => {
-      console.log(model);
-      const generatedHTML = $(detailsTemplate(trip.attributes));
-      detailsElement.html('');
-      detailsElement.append(generatedHTML);
-    }
-  });
+  // trip.fetch({
+  //   success: (model) => {
+  //     const generatedHTML = $(detailsTemplate(trip.attributes));
+  //     detailsElement.append(generatedHTML);
+  //   }
+  // });
 
+  // API doesn't happen if model has already been clicked on and updated
+  if (trip.get('about')) {
+    const generatedHTML = $(detailsTemplate(trip.attributes));
+    detailsElement.append(generatedHTML);
+  } else {
+    trip.fetch({
+      success: (model) => {
+        const generatedHTML = $(detailsTemplate(trip.attributes));
+        detailsElement.append(generatedHTML);
+      }
+    });
+  }
 };
 
 const render = function render(tripList) {
@@ -43,25 +53,29 @@ const render = function render(tripList) {
   tripList.forEach((trip) => {
     const generatedHTML = $(tripTemplate(trip.attributes));
     generatedHTML.on('click', (event) => {
-      // console.log(`clicked on ${trip}`);
-      //
-      // console.log(trip);
       renderDetails(trip);
-
     });
     tripTableElement.append(generatedHTML);
   });
 };
 
 
+
 $(document).ready( () => {
   detailsTemplate = _.template($('#details-template').html());
   tripTemplate = _.template($('#trip-template').html());
-
 
   // adding new models to a collection triggers an update event
   tripList.on('update', render);
 
   // get all of the trips from the API
   tripList.fetch();
+
+  let myTrip = new Trip ({
+    "continent":"Africa",
+    "category":"everything",
+    "weeks":5,
+    "cost":9599.99
+  });
+  console.log(myTrip.isValid());
 });
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -1,9 +1,44 @@
 import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
-  // default: {
-  //   author: 'Unknown'
-  // }
+
+//   cost: params[:cost]
+
+// validates :cost, presence: true, numericality: { greater_than: 0 }
+
+  validate(attributes) {
+    const CONTINENTS = ['Africa', 'Antartica', 'Asia', 'Australasia', 'Europe', 'North America', 'South America']
+
+    const errors = {};
+
+    if (!attributes.name) {
+      errors.title = ['cannot be blank'];
+    }
+    if (!CONTINENTS.includes(attributes.continent)){
+      errors.continent = ["that isn't a continent"];
+    }
+    if (!attributes.continent){
+      errors.continent = ['cannot be blank'];
+    }
+    if (!attributes.about) {
+      errors.about = ['cannot be blank'];
+    }
+    if (!attributes.category) {
+      errors.category = ['cannot be blank'];
+    }
+    if (!attributes.weeks) {
+      errors.weeks = ['cannot be blank'];
+    }
+    if (attributes.weeks <= 0) {
+      errors.weeks = ['trips must be greater than one week in length'];
+    }
+    if (!attributes.cost) {
+      errors.cost = ['cannot be blank'];
+    }
+    if (attributes.cost <= 0) {
+      errors.cost = ['A trips cost must be greater than 0'];
+    }
+  }
 });
 
 
