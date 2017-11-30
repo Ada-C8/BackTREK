1872def840a7fcb68e2a95ed147fb37046b98487
Amy Lee <amy.yj.lee@gmail.com>
2017-11-30 14:55:32 -0800

    added model validations for Trip

--- src/app.js
+++ src/app.js
@@ -36,6 +36,8 @@ const tripList = new TripList();
 //   console.log('called TripInfo');
 //   console.log(this);
 // };
+const formFields = ['name', 'continent', 'category', 'cost', 'about', 'weeks'];
+
 const events = {
   allTrips(event) {
     $('#all-trips').empty();
@@ -77,6 +79,129 @@ const events = {
     // });
 
   },
+  reservationForm(event) {
+    console.log(this);
+    const tripID = this.getAttribute("data-trip-id");
+    console.log(`You've clicked the make reservation button!`);
+    console.log(`you are reserving this trip with id ${tripID}`);
+    const reservationForm = `<form id= "reservation-form" action="https://trektravel.herokuapp.com/trips/${tripID}/reservations" method="post">
+    <section>
+      <label>Name</label>
+      <input type="text" id="name" name="name"></input>
+      <label>Email</label>
+      <input type="text" id="email" name="Email"></input>
+    </section>
+
+    <section class="button">
+      <button type="submitReservation">Reserve My Spot</button>
+    </section>
+    </form>`;
+    $('#trip-details').append(reservationForm);
+  },
+  finalizeReservation(event) {
+    event.preventDefault();
+    console.log('you have submitted your form');
+    const formData = $(this).serialize();
+    const url = $(this).attr('action');
+    $.post(url, formData, (response) => {
+      $('#messages').html(`<h3 id= "status-message"> Successfully reserved this trip for ${response.name}</h3>`);
+      console.log(`Success! You're on the list.`);
+      $('#trip-info').toggle();
+      $('#trip-details form:last-child').empty();
+    }).fail(() => {
+      $('#messages').html('<h3 id= "status-message"> Sorry, there are no spots left for this trip.</h3>');
+      console.log(`Sorry, no spots left.`);
+    });
+  },
+  newTripForm(event) {
+    const tripForm = `
+    <h2> Add New Trip </h2>
+    <form id= "new-trip-form" action="https://trektravel.herokuapp.com/trips" method="post">
+    <section>
+      <label>Name:</label>
+      <input type="text" id="name" name="name"></input>
+      <label>Category:</label>
+      <input type="text" id="category" name="category"></input>
+      <label>Continent:</label>
+      <input type="text" id="continent" name="continent"></input>
+      <label>Cost:</label>
+      <input type="integer" id="cost" name="cost"></input>
+      <label>Weeks:</label>
+      <input type="integer" id="weeks" name="weeks"></input>
+      <label>About:</label>
+      <input type="text" id="about" name="about"></input>
+    </section>
+
+    <section class="button">
+      <button type="submit-new-trip">Add New Trip</button>
+    </section>
+    </form>`;
+    $('#trip-details').empty();
+    console.log('adding new trip form to page');
+    $('#trip-details').append(tripForm);
+  },
+  addTrip(event) {
+    event.preventDefault();
+    const tripData = {};
+    formFields.forEach((field) => {
+      const formValue = $(`input[name=${field}]`).val();
+      if (formValue && formValue != ' ') {
+        tripData[field] = formValue;
+      }
+    });
+
+    const trip = new Trip(tripData);
+    console.log('testing: this is the new trip');
+    console.log(trip);
+
+    if (trip.isValid()) {
+      console.log('this trip is valid')
+      tripList.add(trip);
+      trip.save({}, {
+        success: event.successfulSave,
+        error: events.failedSave,
+      });
+    } else {
+      console.log('there was a client error for this trip');
+      console.log(trip);
+      const errorTypes = Object.keys(trip.validationError);
+      console.log(' the error types we got are:');
+      console.log(errorTypes);
+
+      $('#messages ul').empty();
+
+      errorTypes.forEach((type) => {
+        const eMessages = trip.validationError[type];
+        eMessages.forEach((message) => {
+          $('#messages ul').append(`<li> ${message}</li>`);
+        });
+      });
+
+      $('#messages').show();
+    }
+  },
+  successfulSave(trip, response) {
+    console.log('success! the successfulsave method worked');
+    // console.log(trip);
+    // console.log(response);
+    $('#messages ul').empty();
+    $('#messages ul').append(`<li>${trip.get('name')} succesfully added!</li>`);
+    $('#messages').show();
+  },
+  failedSave(trip, response) {
+    console.log('fail :( we are in the failedSave method');
+    console.log(trip);
+    console.log(response);
+    $('#messages ul').empty();
+    const errs = response.responseJSON.errors;
+    for (let key in errs) {
+      errs[key].forEach((error) => {
+        $('#messages ul').append(`<li>${key}: ${error}</li>`);
+      })
+    }
+    $('#messages').show();
+    trip.destroy();
+  },
 };
 const render= function render(tripList) {
   $('#trip-list').empty();
@@ -93,10 +218,12 @@ $(document).ready( () => {
   tripTemplate = _.template($('#trip-template').html());
   tripList.fetch();
   showTemplate = _.template($('#show-template').html());
-  // $('#all-trips-btn').on('click', () => {
-  //   allTrips();
-  // });
   $('#all-trips-btn').click(events.allTrips);
   $('#all-trips').on('update', render);
   $('#all-trips').on('click', 'tr', events.tripInfo);
+  $('#trip-details').on('click', '#reserve-btn', events.reservationForm);
+  $('#trip-details').on('submit', '#reservation-form', events.finalizeReservation);
+  $('body').on('click', '#add-trip-btn', events.newTripForm);
+  $('#trip-details').on('submit', '#new-trip-form', events.addTrip);
+  // $('#add-trip-btn').click(events.newTripForm);
 });
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -2,6 +2,124 @@ import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
   urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips',
+  validate: function(attributes) {
+    console.log('the attributes of this trip are as follows:');
+    console.log(attributes);
+    const errors = {};
+    const continents = ['africa', 'antartica', 'asia', 'australasia', 'europe', 'north america', 'south america'];
+    const numericFields = ['weeks', 'cost'];
+
+    // const initializeArrays = (field, inputHash) => {
+    //   if (!inputHash[field]) {
+    //     inputHash[field] =[];
+    //   }
+    // };
+
+    if (!attributes.name) {
+      if (!errors['name']) {
+        errors['name'] = [];
+      }
+      // initializeArrays('name', errors);
+      errors['name'].push('Trip must have a name');
+    }
+
+    if (!attributes.continent) {
+      if (!errors['continent']) {
+        errors['continent'] = [];
+      }
+      errors['continent'].push('Trip must have a continent.Continent must be one of the following: Africa, Antartica, Asia, Australasia, Europe, North America, or South America');
+    }
+
+    if (!continents.includes(attributes.continent.toLowerCase())) {
+      if (!errors['continent']) {
+        errors['continent'] = [];
+      }
+
+      errors['continent'].push('Continent must be one of the following: Africa, Antartica, Asia, Australasia, Europe, North America, or South America.');
+    }
+
+    if (!attributes.category) {
+      if (!errors['category']) {
+        errors['category'] = [];
+      }
+      errors['category'].push('Trip must have a category');
+    }
+
+    if (!attributes.about) {
+      if (!errors['about']) {
+        errors['about'] = [];
+      }
+      // initializeArrays('about', errors);
+      errors['about'].push('Trip must have a name');
+    }
+
+    if (!attributes.weeks) {
+      if (!errors['weeks']) {
+        errors['weeks'] = [];
+      }
+      errors['weeks'].push('Number of weeks must be included');
+    }
+
+    if (isNaN(attributes.weeks)) {
+      if (!errors['weeks']) {
+        errors['weeks'] = [];
+      }
+      errors['weeks'].push('Number of weeks must be a valid number');
+    } else if (parseInt(attributes.weeks) <= 0) {
+      if (!errors['weeks']) {
+        errors['weeks'] = [];
+      }
+      errors['weeks'].push('Number of weeks must be greater than 0');
+    }
+
+    if (!attributes.cost) {
+      if (!errors['cost']) {
+        errors['cost'] = [];
+      }
+      errors['cost'].push('Cost must be included');
+    }
+
+    if (isNaN(attributes.cost)) {
+      if (!errors['cost']) {
+        errors['cost'] = [];
+      }
+      errors['cost'].push('Cost must be a valid price');
+    } else if (parseInt(attributes.cost) <= 0) {
+      if (!errors['cost']) {
+        errors['cost'] = [];
+      }
+      errors['cost'].push('Cost must be greater than 0');
+    }
+
+    // numericFields.forEach((numField) => {
+    //   if (!attributes[numField]) {
+    //     if (!errors[numField]) {
+    //       errors[numField] = [];
+    //     }
+    //     errors[numField].push(`${numField} must be included`);
+    //   }
+    //
+    //   if (isNaN(attributes[numField])) {
+    //     if (!errors[numField]) {
+    //       errors[numField] = [];
+    //     }
+    //     errors[numField].push(`${numField} must be a valid number`);
+    //   } else if (parseInt(attributes[numField]) <= 0) {
+    //     if (!errors[numField]) {
+    //       errors[numField] = [];
+    //     }
+    //     errors[numField].push('Cost must be greater than 0');
+    //   }
+    // })
+
+    if (Object.keys(errors).length > 0) {
+      console.log(errors);
+      return errors;
+    } else {
+      return false;
+    }
+  },
+
 });
 
 export default Trip;
