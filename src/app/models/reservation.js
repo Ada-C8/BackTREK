3479b08878c3a170337f77491a570f24b6c87182
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-30 09:36:21 -0800

    remove header bgcolor

--- src/app/models/reservation.js
+++ src/app/models/reservation.js
@@ -1,9 +1,13 @@
 import Backbone from 'backbone';
 
 const Reservation = Backbone.Model.extend({
-  initialize(attr) {
-    this.url = `https://ada-backtrek-api.herokuapp.com/trips/${attr.tripID}/reservations`;
+  // initialize(attr) {
+  //   this.url = `https://ada-backtrek-api.herokuapp.com/trips/${attr.tripID}/reservations`;
+  // },
+  urlRoot() {
+    return `https://ada-backtrek-api.herokuapp.com/trips/${ this.get('tripID') }/reservations`;
   },
+  // id: tripID
 });
 
 export default Reservation;
