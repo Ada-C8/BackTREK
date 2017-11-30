471d6b52aba280c75137f3861217b12ce09cb527
Sairagul Abdukhalieva <abdukhalieva@gmail.com>
2017-11-30 13:57:05 -0800

    Wave1, gives all trips

--- src/app.js
+++ src/app.js
@@ -6,8 +6,56 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+// Our components
+// import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list';
 console.log('it loaded!');
 
-$(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
-});
+// const TRIP_FIELDS = ['id', 'name', 'continent', 'category', 'weeks', 'cost'];
+const tripList = new TripList();
+// const trip = new Trip();
+
+let tripsTemplate;
+
+const render = function render(tripList){
+  const tripsTableElement = $('#trip-list');
+  $('thead').hide();
+
+  tripsTableElement.html('');
+
+
+  tripList.forEach((trip) => {
+    const generatedHTML = $(tripsTemplate(trip.attributes));
+    generatedHTML.on('click',(event) => {
+      renderTrip(trip)
+      $('#trips').hide();
+      $('#trip').show();
+    }); //.on
+    tripsTableElement.append(generatedHTML);
+  }); //tripList
+};  // render
+  const renderTrip = function render(trip){
+    const tripTableElement = $('#trip');
+    tripTableElement.html('');
+
+    const generatedHTML = $(tripsTemplate(trip.attributes));
+    tripTableElement.html(generatedHTML);
+  }
+
+  // const loadtrip = function loadtrip(){
+  //   trip.fetch();
+  //   trip.on('update', renderTrip, trip);
+  // }
+
+
+  $(document).ready(() => {
+    tripsTemplate = _.template($('#trips-template').html());
+    $('#trip-list').hide();
+    $('#load').on('click', function(){
+      $('#trip-list').show();
+    });
+    tripList.fetch();
+    tripList.on('update',render);
+
+
+  });
---
+++ src/app/models/trip.js
@@ -0,0 +1,27 @@
+import Backbone from 'backbone';
+
+const Trip = Backbone.Model.extend({
+  defaults: {
+    name: 'Unknown'
+  },
+//urlRoot(){
+//return `https://ada-backtrek-api.herokuapp.com/trips/${this.get('tripID')}/reservations`
+//}
+  // validate(attributes) {
+  //   const errors = {};
+  //   if (!attributes.name) {
+  //     errors.name = ['cannot be blank'];
+  //   }
+  //
+  //   if (!attributes.email) {
+  //     errors.email = ['cannot be blank'];
+  //   }
+  //
+  //   if (Object.keys(errors).length < 1) {
+  //     return false;
+  //   }
+  //   return errors;
+  // },
+});
+
+export default Trip;
