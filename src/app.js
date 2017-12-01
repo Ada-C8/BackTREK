dc9d6b841244c14570e40771c7100981d48b7dc4
Sairagul Abdukhalieva <abdukhalieva@gmail.com>
2017-12-01 11:55:37 -0800

    Reservation has been added

--- src/app.js
+++ src/app.js
@@ -1,64 +1,72 @@
-// Vendor Modules
-import $ from 'jquery';
-import _ from 'underscore';
+  // Vendor Modules
+  import $ from 'jquery';
+  import _ from 'underscore';
 
-// CSS
-import './css/foundation.css';
-import './css/style.css';
+  // CSS
+  import './css/foundation.css';
+  import './css/style.css';
 
-// Our components
-import Trip from './app/models/trip';
-import TripList from './app/collections/trip_list';
-console.log('it loaded!');
+  // Our components
+  import Trip from './app/models/trip';
+  import Reservation from './app/models/reservation';
+  import TripList from './app/collections/trip_list';
+  console.log('it loaded!');
 
-const TRIP_FIELDS = ['name', 'continent', 'about', 'category', 'weeks', 'cost'];
+  const TRIP_FIELDS = ['name', 'continent', 'about', 'category', 'weeks', 'cost'];
+  const RESERVATION_FIELDS = ['name', 'age', 'email'];
 
-let tripsTemplate;
+  let tripsTemplate;
+  let tripTemplate;
 
-const render = function render(tripList){
-  const tripsTableElement = $('#trip-list');
-  tripsTableElement.html('');
+  const render = function render(tripList){
+    const tripsTableElement = $('#trip-list');
+    tripsTableElement.html('');
 
-  tripList.forEach((trip) => {
-    const generatedHTML = $(tripsTemplate(trip.attributes));
-    generatedHTML.on('click',(event) => {
-      renderTrip(trip)
-      $('#trips').hide();
-      $('#trip').show();
-    }); //.on
-    tripsTableElement.append(generatedHTML);
-  }); //tripList
-};  // render
+    tripList.forEach((trip) => {
+      const generatedHTML = $(tripsTemplate(trip.attributes));
+      generatedHTML.on('click',(event) => {
+        trip.fetch({
+          success: function(model, response){
+            renderTrip(model);
+            $('#trips').hide();
+            $('#trip').show();
+          }
+        });
+      }); //.on
+      tripsTableElement.append(generatedHTML);
+    }); //tripList
+  };  // render
 
 
   const renderTrip = function render(trip){
     const tripTableElement = $('#trip');
     tripTableElement.html('');
 
-    const generatedHTML = $(tripsTemplate(trip.attributes));
+    const generatedHTML = $(tripTemplate(trip.attributes));
     tripTableElement.html(generatedHTML);
   };
 
 
   $(document).ready(() => {
     tripsTemplate = _.template($('#trips-template').html());
+    tripTemplate = _.template($('#trip-template').html());
+
     const tripList = new TripList();
     tripList.on('update',render);
+
     $('#load').on('click', function(){
-      $('#trip-list').show();
+      // $('#trip-list').show();
       tripList.fetch({
-        success: function(){
+        success: function(collection, response){
           $('#trips').show();
+          $('#trip').hide();
         }
       });
     });
 
-    // tripTemplate = _.template($('#trip-template').html());
-    // const loadtrip = function loadtrip(){
-    //   trip.fetch();
-    //   trip.on('update', renderTrip, trip);
-    // }
-
+    // $('#add_trip').on('click', function(){
+    //   $('#show_form').show();
+    // };
     $("#add-trip-form").on('submit', function(event){
       event.preventDefault(); // stops after reading user input, doesn't do anything yet
       let tripData = {};
@@ -73,7 +81,29 @@ const render = function render(tripList){
         success: function(model, response){
           tripList.add(model);
         }
+      })
+    });
+
+    $("#trip").on('submit', "#reservation-form", function(event){
+      event.preventDefault();
+      let reservationData = {};
+
+      RESERVATION_FIELDS.forEach((field) => {
+        reservationData[field] = $(`#reservation-form input[name="${ field }"]`).val();
       });
 
+      let reservation = new Reservation(reservationData);
+      console.log(`hello ${reservation}`);
+      reservation.set('trip_id', $(this).data('tripId'));
+      console.log(`hello ${reservation}`);
+
+      reservation.save({}, {
+        success: function(model, response){
+          console.log(`Thank you, your reservation has been placed successfully`);
+        }
+      });
     });
+
   });
+
+ // for ${reservationData['name']}
--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -1,12 +1,10 @@
 import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
+  urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips/',
   defaults: {
     name: 'Unknown'
-  },
-urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips/'
-
-
+  }
 // urlRoot(){
 // return `https://ada-backtrek-api.herokuapp.com/trips/${this.get('tripID')}/reservations`
 // }
