1b0e26e9551a5182695b96197af93f7c628680c4
Isaac Del Rosario <cecdelr@gmail.com>
2017-12-01 10:33:25 -0800

    Trimmed leading/trailing spaces in attributes with string because a trip shouldn't begin with emptiness.

--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -5,6 +5,13 @@ const TripList = Backbone.Collection.extend({
   model: Trip,
   url: 'https://ada-backtrek-api.herokuapp.com/trips/',
   parse(response) {
+    response.forEach(function(tripAttributes) {
+      // ' Sloop John B.' made me realize leading/trailing spaces are a thing
+      // Backbone considers spaces when alphabetizing
+      tripAttributes.name = tripAttributes.name.trim();
+      tripAttributes.continent = tripAttributes.continent.trim();
+      tripAttributes.category = tripAttributes.category.trim();
+    });
     return response;
   }
 });
