c5506bb575ffd95f37008e2a46cb678dc2cf1339
Tanisha <tanishahampden@gmail.com>
2017-11-30 21:05:58 -0800

    add urlRoot and toString override

--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -1,6 +1,13 @@
 import Backbone from 'backbone'
 
 const Trip = Backbone.Model.extend({
+  urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips',
+  // urlRoot() {
+  //   return `https://ada-backtrek-api.herokuapp.com/trips/${ this.get('tripId') }/reservations`;
+  // },
+  toString() {
+    return `<Trip ${ this.get('name') }>`;
+  },
 
 });
 
