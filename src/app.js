94f54e38c9a76d1b8e5a0a5558e7138b070a9f83
Lauren <Laurenelee88@gmail.com>
2017-12-01 12:17:44 -0800

    beginning to clearn up functions

--- src/app.js
+++ src/app.js
@@ -19,16 +19,78 @@ let tripTemplate;
 let individualTripTemplate;
 let reserveTemplate;
 
-console.log('it loaded!');
-
+//////////////////////////
+// Status reporting
+//////////////////////////
 const clearStatus = function clearStatus() {
   $('#status-messages ul').html();
-  $('#status-messages').hide(); //how we hide this
+  $('#status-messages').hide();
 };
 const reportStatus = function reportStatus(status, message) {
   const statusHTML = `<li class="status${ status }">${ message }</li>`;
   $('#status-messages ul').append(statusHTML);
-  $('#status-messages').show(); // now you're showing message
+  $('#status-messages').show();
+};
+
+//////////////////////////
+// Reservation Details ///
+//////////////////////////
+const addReservation = function addReservation(trip, formElement){
+  let form = formElement;
+  let formData = formElement.serialize(); // dont use serialize
+  // read form otherwise and use data and create instance of reservation. and pass in tripID and call .save on that
+  const success = function success(){
+    $('#add-reservation').hide();
+    reportStatus('success', 'Reservation Made!');
+    console.log('success');
+    form.find("input[type=text], input[type=number]").val('');
+  };
+
+  let url = `https://ada-backtrek-api.herokuapp.com/trips/${ trip.id }/reservations`;
+  // const reservation = new Reservation( {tripId: trip.id});
+
+  $.post(url, formData, success)
+  .fail(function(){
+    console.log('failure');
+    reportStatus('error', 'Reservation failed!');
+  });
+};
+//////////////////////////
+// Trip Details
+//////////////////////////
+const showTripDetails = function showTripDetails(trip){
+  console.log(trip.attributes); // has the about
+  const individualTripListElement = $('#individual-trip-detail');
+  individualTripListElement.html('');
+
+  const generatedHTMLTripDetails = individualTripTemplate(trip.attributes);
+
+  individualTripListElement.append(generatedHTMLTripDetails);
+  $('#individual-trip-detail').show();
+
+  //////////////////////////
+  // reservation handler
+  //////////////////////////
+  $('#reserve').show(); //button display
+  $('#reserve').on('click', function(event) {
+    $('#add-reservation').show();
+    $('#status-messages').html('');
+  });
+  $('#add-reservation').on('submit', function(event) {
+    event.preventDefault();
+    addReservation(trip, $(this));
+  });
+};
+// pulled out fetchTripDetails function above
+const fetchTripDetails = function fetchTripDetails(event) {
+  $('#add-reservation').hide();
+  let tripId = $(this).attr('data-id');
+  let trip = tripList.get(tripId)
+  trip.fetch({
+    success: () => {
+      showTripDetails(trip);
+    }
+  });
 };
 
 
@@ -37,70 +99,14 @@ const render = function render(tripList) {
   tripListElement.empty();
 
   tripList.forEach((trip) => {
-    // console.log(`Rendering ${ trip.get('name') }`);
     const generatedHTML = tripTemplate(trip.attributes);
     tripListElement.append($(generatedHTML));
 
-    // console.log(`Rendering trip *** ${ trip }`); //testing custom method toString()
   });
   $('th.sort').removeClass('current-sort-field');
   $(`th.sort.${ tripList.comparator }`).addClass('current-sort-field');
 
-  //// individual trip detail below /////
-  $('.trip').on('click',function(event) {
-    $('#add-reservation').hide();
-    let tripId = $(this).attr('data-id');
-    let trip = tripList.get(tripId)
-    trip.fetch({
-      success: function() {
-        console.log(trip.attributes); // has the about
-        const individualTripListElement = $('#individual-trip-detail');
-        individualTripListElement.html('');
-
-        const generatedHTMLTripDetails = individualTripTemplate(trip.attributes);
-
-        individualTripListElement.append(generatedHTMLTripDetails);
-
-        $('#individual-trip-detail').show();
-        // handling reservation!
-        $('#reserve').show(); //button display
-        $('#reserve').on('click', function(event) {
-          $('#add-reservation').show();
-          $('#messages').html('');
-        });
-        $('#add-reservation').on('submit', function(event){
-
-
-          event.preventDefault();
-          let form = $(this)
-          console.log('in submit event');
-          let formData = $(this).serialize();
-          const reservationResponse = function reservationResponse(status) {
-            // console.log('inside res response');
-            $("#messages").html(`<h3> ${status} </h3>`);
-          };
-          const positive = 'Resevation Made!';
-          const success = function success(){
-            $('#add-reservation').hide();
-            reservationResponse(positive);
-            form.find("input[type=text], input[type=number]").val('');
-          };
-          const negative = 'Reservation failed';
-          let url = `https://ada-backtrek-api.herokuapp.com/trips/${ tripId }/reservations`;
-          console.log(formData);
-          $.post(url, formData, success)
-          .fail(function(){
-            console.log('failure');
-            reservationResponse(negative);
-          })
-          .always(function(){
-            console.log('always even if we have success or failure');
-          });
-
-        });
-      }
-    }); // fetch
-  });
+  $('.trip').on('click', fetchTripDetails);
 };
 
 const readFormData = function readFormData() {
@@ -108,13 +114,11 @@ const readFormData = function readFormData() {
   TRIP_FIELDS.forEAch((field) => {
     const inputElement = $(`#add-trip-form input[name=${ field }]`);
     const value = inputElement.val();
-    if (value != '') { //dont take empty strings so that backbone can fill in default values
+    if (value != '') {
       tripDarta[field] = value;
     }
     inputElement.val('');
   });
-  console.log("Read trip data");
-  console.log(tripData);
   return tripData;
 };
 
@@ -127,7 +131,6 @@ const handleValidationFailures = function handleValidationFailures(errors) {
 };
 const addTripHandler = function(event) {
   event.preventDefault();
-  // const trip = tripList.add(readFormData);
   const trip = Trip(readFormData);
   if (!trip.isValid()) {
     handleValidationFailures(trip.validationError);
@@ -153,7 +156,8 @@ const addTripHandler = function(event) {
 $(document).ready( () => {
   individualTripTemplate = _.template($('#individual-trip-template').html());
   tripTemplate = _.template($('#trip-template').html());
-  reserveTemplate = _.template($('#reserve-trip-template').html());
+  // reserveTemplate = _.template($('#reserve-trip-template').html());
+  // 11/30/17 not using this template
 
   tripList.on('update', render);
   tripList.on('sort', render);
@@ -171,4 +175,25 @@ $(document).ready( () => {
   $('#status-messages button.clear').on('click', clearStatus);
   $('#reserve').hide();
   $('#add-reservation').hide();
+  // ///////// MODAL ///////// in doc ready? ///////
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 });
--- src/app/models/reservation.js
+++ src/app/models/reservation.js
@@ -3,21 +3,8 @@ import Trip from '../models/trip';
 import TripList from '../models/reservation';
 
 const Reservation = Backbone.Collection.extend({
-  model: Trip,
-
-
-  // const tripId = this.get('tripId');
-  // urlRoot: `http://localhost:3000/trips/`,
-
-  // ${ tripId }/reservations`;
-
-  urlRoot: function(tripId) {
-    // const tripId = this.get('tripId');
-    // if (isNaN(tripId)) {
-    //   throw 'Cannot make a reservation w/o trip ID';
-    // }
-    return `http://localhost:3000/trips/${ tripId }/reservations`;
+  urlRoot: function() {
+    return `http://localhost:3000/trips/ ${this.get('tripId')}/reservations`;
   },
-
 });
 export default TripList;
