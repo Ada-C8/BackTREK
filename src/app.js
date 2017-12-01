5aba1f31695d35b4276e87dc9c4d98b354e40db0
VirtualLindsey <tapircode@gmail.com>
2017-12-01 04:59:21 -0800

    finsihing up wave 3 and logic edits

--- src/app.js
+++ src/app.js
@@ -13,8 +13,6 @@ const TRIP_FIELDS = ['name', 'category', 'weeks', 'cost', 'about', 'continent']
 let tripTemplate;
 
 const loadTrips = function loadTrips(trips) {
-  console.log('function working');
-  console.log(trips);
   const tripTable = $('#tripSection');
   tripTable.html('');
   trips.forEach((trip) => {
@@ -29,69 +27,121 @@ $(document).ready( () => {
   const tripList = new TripList();
   tripList.fetch();
 
+  $('#singleTripSection').hide();
+  $('#reservationForm').hide();
+  $('#createTripSection').hide();
+  $('#numberFilter').hide();
 
-  $('#tripButton').on('click',() => {
-    console.log('button works');
-    tripList.forEach((trip) => {
-      // const newTrip = new Trip(trip);
-      const generatedHTML = tripTemplate(trip.attributes);
-      $('#tripSection').append($(generatedHTML));
-    });
-  });
 
   $('#tripSection').on('click', 'tr', (e) => {
-    console.log('single trip button worked');
+    $('#singleTripSection').show();
+    $('#reservationForm').show();
     const baseURL = 'https://ada-backtrek-api.herokuapp.com/trips/'
     $.get(`${baseURL}${e.currentTarget.id}`, (response) => {
-      console.log(response);
       const generatedHTML = singleTripTemplate(response);
-      console.log(generatedHTML);
       $('#singleTrip').html(generatedHTML);
+    }).fail(() => {
+      console.log('The post call failed');
     });
   });
 
-  tripList.on('sort', loadTrips);
-  tripList.on('update', loadTrips)
+  tripList.on('sort', loadTrips, tripList);
+  tripList.on('update', loadTrips, tripList);
+
+  $('#createTripButton').on('click', () => {
+    $('#createTripSection').show();
+  });
 
   $('#createTripForm').on('submit', (e) => {
-    console.log('Submission started');
     e.preventDefault();
+    let valid = true;
+    if ($('#nameField').val() === '') {
+      $('#nameField').css('background-color','red');
+      valid = false;
+    } else {
+      $('#nameField').css('background-color','white');
+    }
 
-    const url = 'https://ada-backtrek-api.herokuapp.com/trips';
-    const data = $('#createTripForm').serialize();
+    if ($('#aboutField').val() === '') {
+      $('#aboutField').css('background-color','red');
+      valid = false;
+    } else {
+      $('#aboutField').css('background-color','white');
+    }
 
-    $.post(url, data, (response) => {
-      console.log('POST worked');
-      console.log(response);
-    }).fail(() => {
+    if ($('#categoryField').val() === '') {
+      $('#categoryField').css('background-color','red');
+      valid = false;
+    } else {
+      $('#categoryField').css('background-color','white');
+    }
 
-      console.log('The post call failed');
-    });
-    return false;
+    if ($('#weeksField').val() === '') {
+      $('#weeksField').css('background-color','red');
+      valid = false;
+    } else {
+      $('#weeksField').css('background-color','white');
+    }
+
+    if ($('#costField').val() === '') {
+      $('#costField').css('background-color','red');
+      valid = false;
+    } else {
+      $('#costField').css('background-color','white');
+    }
+
+    if (valid){
+      const url = 'https://ada-backtrek-api.herokuapp.com/trips';
+      const data = $('#createTripForm').serialize();
+
+      $.post(url, data, (response) => {
+        const newTrip = new Trip(response);
+        tripList.add(newTrip);
+        $('#createTripSection').hide();
+      }).fail(() => {
+        console.log('The post call failed');
+      });
+      return false;
+    }
   });
 
   $('#reservationForm').on('submit', () => {
-    console.log('Submission started');
-    const id = $('#singleTrip li')[0].id;
+    let valid = true;
+    if ($('#reservationName').val() === '') {
+      $('#reservationName').css('background-color','red');
+      valid = false;
+    } else {
+      $('#reservationName').css('background-color','white');
+    }
 
-    const url = `https://ada-backtrek-api.herokuapp.com/trips/${id}/reservations`
-    const data = $('#reservationForm').serialize();
-    console.log('the data is');
+    if ($('#reservationEmail').val() === '') {
+      $('#reservationEmail').css('background-color','red');
+      valid = false;
+    } else {
+      $('#weeksField').css('background-color','white');
+    }
 
-    $.post(url, data, (response) => {
-      console.log('POST worked');
-      console.log(response);
+    if (valid) {
+      const id = $('#singleTrip li')[0].id;
 
-    }).fail(() => {
-      console.log('The post call failed');
-    });
+      const url = `https://ada-backtrek-api.herokuapp.com/trips/${id}/reservations`
+      const data = $('#reservationForm').serialize();
+
+      $.post(url, data, () => {
+        $('#reservationName').val('');
+        $('#reservationEmail').val('');
+        $('#reservationForm').hide();
+        $('#singleTripSection').hide();
+      }).fail(() => {
+        console.log('The post call failed');
+      });
+    }
     return false;
   });
 
   TRIP_FIELDS.forEach((field) => {
     const header = $(`.sort.${field}`);
     header.on('click', () => {
-      console.log(`sorting by ${field}`);
       tripList.comparator = field;
       tripList.sort();
     });
@@ -99,22 +149,25 @@ $(document).ready( () => {
 
 
   $('#filter').on('change', () => {
-    console.log('event working');
     const filter = $('#filter')[0].value;
     if (filter === 'blank') {
       return false;
-    } else if (filter === 'cost' || filter === 'weeks') {
+    } else if (filter === 'budget' || filter === 'weeks') {
       $('#textFilter').hide();
+      $('#textFilter').val('');
       $('#numberFilter').show();
+      $('#numberFilter').val('');
     } else {
       $('#textFilter').show();
+      $('#textFilter').html('');
       $('#numberFilter').hide();
+      $('#numberFilter').html('');
     }
+    loadTrips(tripList);
   });
 
   $('#filterForm').on('submit', () => {
     const filter = $('#filter')[0].value;
-
     let value;
     if($('#numberFilter').is(':visible')){
       value = $('#numberFilter')[0].value;
@@ -122,19 +175,17 @@ $(document).ready( () => {
       value = $('#textFilter')[0].value;
     }
 
-
     const url = `https://ada-backtrek-api.herokuapp.com/trips/${filter}?query=${value}`;
-
-    console.log(url);
     $.get(url, (response) => {
-      console.log(response);
+
       $('#tripSection').html('');
       response.forEach((trip) => {
-        console.log(trip.id);
         $.get(`https://ada-backtrek-api.herokuapp.com/trips/${trip.id}`, (response) => {
             console.log(response);
             const generatedHTML = tripTemplate(response);
             $('#tripSection').append($(generatedHTML));
+        }).fail(() => {
+          console.log('The post call failed');
         });
 
       });
@@ -147,31 +198,63 @@ $(document).ready( () => {
       const filter = $('#filter')[0].value;
       const elements = $('#tripSection').children();
 
-      if (value <= 0){
+      if (filter === 'weeks' || filter === 'budget') {
+        if (value <= 0){
+            $('#tripSection tr').each((index) => {
+              const rowID = elements[index].id;
+              $(`tr#${rowID}`).show();
+            });
+        } else {
           $('#tripSection tr').each((index) => {
             const rowID = elements[index].id;
-            $(`tr#${rowID}`).show();
-          });
-      } else {
+            let rowValue;
+            if (filter === 'weeks'){
+              rowValue = $(`tr#${rowID} td:nth-child(5)`).html();
+            } else {
+              rowValue = $(`tr#${rowID} td:nth-child(6)`).html();
+            }
 
-        //console.log($(`tr#${0}`).hide());
-        $('#tripSection tr').each((index) => {
-          //const temp = $(`tr#${index}`).children()[2];
-          const rowID = elements[index].id;
-          const rowValue = $(`tr#${rowID} td:nth-child(3)`).html();
-          if (filter === 'weeks'){
-            console.log("############");
             if (parseInt(rowValue) <= value){
-              console.log(`${rowValue} is less than ${value}`);
               $(`tr#${rowID}`).show();
             } else {
               $(`tr#${rowID}`).hide();
             }
-          }
-        });
+          });
+        }
       }
-
-
   });
 
+  $('#textFilter').on('keyup', () => {
+      const value = $('#textFilter')[0].value.trim().toLowerCase();
+      const filter = $('#filter')[0].value;
+      const elements = $('#tripSection').children();
+
+      if (filter === 'name' || filter === 'continent' || filter === 'category') {
+        if (value === "") {
+            $('#tripSection tr').each((index) => {
+              const rowID = elements[index].id;
+              $(`tr#${rowID}`).show();
+            });
+        } else {
+          $('#tripSection tr').each((index) => {
+            const rowID = elements[index].id;
+
+            let rowValue;
+            if (filter === 'name'){
+              rowValue = $(`tr#${rowID} td:nth-child(2)`).html()
+            } else if (filter === 'category') {
+              rowValue = $(`tr#${rowID} td:nth-child(3)`).html();
+            } else {
+              rowValue = $(`tr#${rowID} td:nth-child(4)`).html();
+            }
+            rowValue = rowValue.toLowerCase();
+            if (rowValue.indexOf(value) >= 0){
+              $(`tr#${rowID}`).show();
+            } else {
+              $(`tr#${rowID}`).hide();
+            }
+          });
+        }
+      }
+  });
 });
