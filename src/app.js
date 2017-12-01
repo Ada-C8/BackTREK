4b0cfca539383ef2f7bcce63a71cfc11bbab4e37
enigmagnetic <lauren.cardella@gmail.com>
2017-12-01 02:04:45 -0800

    Add templates for status messages, trips table rows, and trip details. Add basic HTML structure. Build loadTrips() function to retrieve trip collection. Build Trip model and TripList Collection.

--- src/app.js
+++ src/app.js
@@ -6,8 +6,43 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+import Trip from './app/models/trip';
+import TripList from './app/collections/trip_list';
+
+let tripList;
+let tripTemplate;
+let tripDetailsTemplate;
+let statusMessageTemplate;
+
 console.log('it loaded!');
 
+const render = function render(tripList) {
+  const tripListElement = $('#trip-list');
+  tripListElement.empty();
+  console.log(tripList);
+
+  tripList.forEach((trip) => {
+    tripListElement.append(tripTemplate(trip.attributes));
+  });
+};
+
+const loadTrips = function loadTrips() {
+  tripList.fetch();
+  tripList.on('update', render, tripList);
+  $('#trips').show();
+};
+
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  $('#trips').hide();
+  tripTemplate = _.template($('#trip-template').html());
+  tripDetailsTemplate = _.template($('#trip-details-template').html());
+  statusMessageTemplate = _.template($('#status-message-template').html());
+
+  tripList = new TripList();
+
+  $('button#search').on('click', loadTrips);
+  $('#trip-list').on('click', 'tr', function() {
+    tripList.fetch($(this).attr('data-id'));
+    console.log('clicked!');
+  });
 });
