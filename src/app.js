e69e99707814a21dc1feae00d54a3b1d7e9f996e
Sara C <srachandler@gmail.com>
2017-11-30 19:39:38 -0800

    added function to render triplist

--- src/app.js
+++ src/app.js
@@ -7,9 +7,27 @@ import './css/foundation.css';
 import './css/style.css';
 
 import Trip from '.app/models/trip';
+import TripList from '.app/collections/trip_list';
 
 console.log('it loaded!');
 
+const tripList = new TripList();
+let tripTemplate;
+
+const render = function render(tripList) {
+
+  // Get the element to append to
+  const $tripList = $('#trip-list');
+  $tripList.empty();
+
+  tripList.forEach((trip) => {
+    $tripList.append(tripTemplate(trip.attributes));
+  });
+};
+
 $(document).ready( () => {
-  $('main').html('<h1>Hello World!</h1>');
+  tripTemplate = _.template($('#trip-template').html());
+  tripList.on('update', render, tripList);
+  tripList.fetch();
+  // $('main').html('<h1>Hello World!</h1>');
 });
