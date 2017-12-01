f1de288abe81c0edcbcabb7a9650fc2ee2876b82
Tanisha <tanishahampden@gmail.com>
2017-12-01 15:15:00 -0800

    update toString override

--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -3,10 +3,10 @@ import Backbone from 'backbone'
 const Trip = Backbone.Model.extend({
   urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips',
   // urlRoot() {
-  //   return `https://ada-backtrek-api.herokuapp.com/trips/${ this.get('tripId') }/reservations`;
+  //   return `https://ada-backtrek-api.herokuapp.com/trips/${ this.get('tripId') }`;
   // },
   toString() {
-    return `<Trip ${ this.get('name') }>`;
+    return `<Trip name:${this.get("name")},continent:${this.get("continent")}, weeks:${this.get("weeks")}, cost:${this.get("cost")} >`;
   },
 
 });
