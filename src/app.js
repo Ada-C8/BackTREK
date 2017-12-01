8ad54c6134a488a7f7cfe8715d55b5ce0a6c6503
Tanisha <tanishahampden@gmail.com>
2017-12-01 10:18:43 -0800

    add renderTrip function

--- src/app.js
+++ src/app.js
@@ -7,27 +7,48 @@ import './css/foundation.css';
 import './css/style.css';
 
 import TripList from 'app/collections/trip_list';
+import Trip from 'app/models/trip'
 
 // const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'cost'];
 
 const tripList = new TripList();
+const adventure = new Trip();
+console.log(`trip trip ${adventure}`);
 
-let tripTemplate;
+let listTemplate;
 
-const render = function render(list) {
+const renderTrips = function render(list) {
   const tripTableElement = $('#trip-list');
   tripTableElement.html('');
   console.log(`tripList :${list}`);
   list.forEach((trip) => {
-    const generatedHTML = tripTemplate(trip.attributes);
+    const generatedHTML = listTemplate(trip.attributes);
+    // console.log(trip.toString());
     tripTableElement.append(generatedHTML);
   });
-
-
 };
 
+let tripTemplate;
+
+const renderTrip = function renderTrip(trip) {
+  const tripElement = $('#trip');
+  // clears html in tripElement
+  tripElement.html('');
+  const generatedHTML = tripTemplate(
+    {name: "name",
+    continent: "continent",
+    weeks: "weeks",
+    cost: "cost",
+    category: "name",
+});
+  tripElement.append(generatedHTML);
+}
+
+
 $(document).ready( () => {
-  // $('main').html('<h1>Hello World!</h1>');
+  // compile underscore list-template
+  listTemplate = _.template($('#list-template').html());
+
   tripTemplate = _.template($('#trip-template').html());
 
   console.log(`About to fetch data from ${ tripList.url }`);
@@ -36,8 +57,8 @@ $(document).ready( () => {
 
   tripList.fetch({
     success: function(list, resp) {
-      console.log('worked');
-      console.log(list);
+      // console.log('worked');
+      // console.log(list);
 
     },
     error: function() {
@@ -45,8 +66,22 @@ $(document).ready( () => {
     }
   });
 
+  console.log(`log trip url : ${trip.url}`);
+   // call .fetch on an instance of a trip
+   // passing the trip into click event function closure thing
+
   $('#trips').on('click', (event) => {
-    console.log('CLICKED!');
-    render(tripList);
-  })
+    renderTrips(tripList);
+
+    $('#trip-name').on('click', (event) => {
+      // grab backbone object and pass into function
+      renderTrip(adventure);
+    })
+
+  });
+
+  // $('td').on('click', 'th', function (){
+  //   console.log(`clicked! the trip name`);
+  // })
+
 });
