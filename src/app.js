50714128b178e6d2eb2bccc90bb9b3956abcf132
Tanisha <tanishahampden@gmail.com>
2017-12-01 15:15:53 -0800

    add trip event handler to renderTrips function

--- src/app.js
+++ src/app.js
@@ -7,81 +7,59 @@ import './css/foundation.css';
 import './css/style.css';
 
 import TripList from 'app/collections/trip_list';
-import Trip from 'app/models/trip'
 
 // const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'cost'];
 
 const tripList = new TripList();
-const adventure = new Trip();
-console.log(`trip trip ${adventure}`);
 
+// initalize templates
 let listTemplate;
+let tripTemplate;
 
-const renderTrips = function render(list) {
+const renderTrips = function renderTrips(list) {
   const tripTableElement = $('#trip-list');
   tripTableElement.html('');
   console.log(`tripList :${list}`);
   list.forEach((trip) => {
-    const generatedHTML = listTemplate(trip.attributes);
-    // console.log(trip.toString());
+    const generatedHTML = $(listTemplate(trip.attributes));
+    // adding event handler here gives context as to which trip needs to be clicked
+    generatedHTML.on('click', (event) => {
+      // grab backbone object and pass into function
+      renderTrip(trip);
+    })
     tripTableElement.append(generatedHTML);
   });
 };
 
-let tripTemplate;
-
 const renderTrip = function renderTrip(trip) {
+  console.log(`trip: ${trip}`);
   const tripElement = $('#trip');
   // clears html in tripElement
   tripElement.html('');
-  const generatedHTML = tripTemplate(
-    {name: "name",
-    continent: "continent",
-    weeks: "weeks",
-    cost: "cost",
-    category: "name",
-});
+  const generatedHTML = tripTemplate(trip.attributes);
   tripElement.append(generatedHTML);
 }
 
-
 $(document).ready( () => {
-  // compile underscore list-template
+  // compile underscore templates
   listTemplate = _.template($('#list-template').html());
-
   tripTemplate = _.template($('#trip-template').html());
 
-  console.log(`About to fetch data from ${ tripList.url }`);
+  tripList.fetch();
 
-  // tripList.on('update', render, tripList);
-
-  tripList.fetch({
-    success: function(list, resp) {
-      // console.log('worked');
-      // console.log(list);
-
-    },
-    error: function() {
-      console.log('error');
-    }
-  });
-
-  console.log(`log trip url : ${trip.url}`);
-   // call .fetch on an instance of a trip
-   // passing the trip into click event function closure thing
+  // tripList.fetch({
+  //   success: function(list, resp) {
+  //     // console.log('worked');
+  //     // console.log(list);
+  //
+  //   },
+  //   error: function() {
+  //     console.log('error');
+  //   }
+  // });
 
   $('#trips').on('click', (event) => {
     renderTrips(tripList);
+  }); // end tripsList event handler
 
-    $('#trip-name').on('click', (event) => {
-      // grab backbone object and pass into function
-      renderTrip(adventure);
-    })
-
-  });
-
-  // $('td').on('click', 'th', function (){
-  //   console.log(`clicked! the trip name`);
-  // })
-
-});
+}); // end doc.ready
