3f828b420964cf1aa51b53c7bb2737accb39439a
Tanisha <tanishahampden@gmail.com>
2017-12-01 10:16:14 -0800

    whitespace

--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -5,8 +5,6 @@ const TripList = Backbone.Collection.extend({
   model: Trip,
   url: 'https://ada-backtrek-api.herokuapp.com/trips',
   parse: function(response) {
-    // it works!
-    console.log('in parse', response);
     return response;
   }
 });
