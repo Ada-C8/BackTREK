3e12509ceb2e4d9cc271107a06a429617afda324
Tanisha <tanishahampden@gmail.com>
2017-12-01 17:01:28 -0800

    update urlRoot

--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -3,10 +3,10 @@ import Backbone from 'backbone'
 const Trip = Backbone.Model.extend({
   urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips',
   // urlRoot() {
-  //   return `https://ada-backtrek-api.herokuapp.com/trips/${ this.get('tripId') }`;
+  //   return `https://ada-backtrek-api.herokuapp.com/trips/${ this.get('id') }`;
   // },
   toString() {
-    return `<Trip name:${this.get("name")},continent:${this.get("continent")}, weeks:${this.get("weeks")}, cost:${this.get("cost")} >`;
+    return `<Trip id:${this.get("id")},name:${this.get("name")},continent:${this.get("continent")}, weeks:${this.get("weeks")}, cost:${this.get("cost")}, about:${this.get("about")}  >`;
   },
 
 });
