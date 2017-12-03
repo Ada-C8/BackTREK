8eafe0871bfe90c1cc9e76c0d25c0a3fc343a180
Amy Cash <amymcash@gmail.com>
2017-12-02 21:01:55 -0800

    Add show trip buttons

--- src/app.js
+++ src/app.js
@@ -12,29 +12,30 @@ console.log('it loaded!');
 import Trip from './app/models/trip';
 import TripList from './app/collections/trip_list';
 
+
+
 const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'price'];
 
 let tripTemplate;
 
-//Methods
+// Render Methods
 
 const renderAll = function renderAll(tripList) {
   const tripListElement = $('#trip-list');
   // make the table-body with id trip-list a jquery object & assign it to a variable
   tripListElement.empty();
   // clear the table-body with id trip-list
-
   // Apply styling to the current sort field
   $('th.sort').removeClass('current-sort-field');
   // remove class from all headings
   $(`th.sort.${ tripList.comparator }` ).addClass('current-sort-field');
   // add class when triggered
 
- // redraw the table
- // tripList.on('update', render);
+  // redraw the table
+  tripList.on('update', renderAll);
 
- // redraw table by sort field
- tripList.on('sort', renderAll);
+  // redraw table by sort field
+  tripList.on('sort', renderAll);
 
   tripList.forEach((trip) => {
     console.log(`Rendering trip ${ trip.get('name') }`);
@@ -66,23 +67,14 @@ const renderAll = function renderAll(tripList) {
 
 $(document).ready( () => {
 
-  console.log('in document ready');
-
   tripTemplate = _.template($('#trip-template').html() ); //same for collections and models
 
   // Builds a collection
   const tripList = new TripList();
 
-  // gets date from APi and triggers an Update Event
-  // $('#load').on('click', )
-  // tripList.fetch();
-
   // tripList.on('update', renderAll);
   tripList.on('sort', renderAll);
   //so the fetch will trigged update and then render will render the data into the template.
-  $('.name').on('click', 'td', () => {
-    console.log("you clicked something");
-  })
 
   //Event handlers for buttons
   const allTrips = $('#load-trips');
@@ -91,6 +83,12 @@ $(document).ready( () => {
     console.log("I fetched it!");
     // tripList.renderAll;
   })
+
+
+  // const oneTrip = $('#');
+  // oneTrip.on('click', () => {
+  //
+  // })
 
   //Event handlers for table headers
   TRIP_FIELDS.forEach((field) => {
@@ -111,40 +109,3 @@ $(document).ready( () => {
 
 
 }); // end document ready
-
-
-  //
-  //
-  //
-  // const tripHTML = tripTemplate(testTrip.attributes);
-  // console.log(tripHTML);
-  // $('#trip-list').append(tripHTML);
-  //
-  // practice data
-
-  // const testTrip = new Trip({
-  //     name: "Cairo",
-  //     continent: "Africa",
-  //     category: "everything",
-  //     weeks: 5,
-  //     cost: 9599.99
-  //   });
-  //
-  // const testTripTwo = new Trip({
-  //   name: "Place",
-  //   continent: "Asia",
-  //   category: "Fun",
-  //   weeks: 2,
-  //   cost: 1000.15
-  // })
-
-
-
-  //
-
-  // const tripList = new TripList();
-  // tripList.add(testTrip);
-  // tripList.add(testTripTwo);
-  //
-  // console.log(tripList.at(0));
-//
