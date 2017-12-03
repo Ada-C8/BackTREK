618c41ffe3739488daf33aea1c34bbf7e6ebf93c
Amy Cash <amymcash@gmail.com>
2017-12-03 11:53:11 -0800

    Add Reservation form

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import TripList from './app/collections/trip_list';
 
 
 const TRIP_FIELDS = ['name', 'continent', 'category', 'weeks', 'price'];
+const RES_FIELDS = ['name', 'email'];
 
 let allTripsTemplate;
 let showDetailsTemplate;
@@ -42,61 +43,93 @@ const sampleHandler = (event) => {
 
 
 const renderDetails = function renderDetails(id) {
-  console.log("in the render and the id is");
-  console.log(id);
   const tripDetails = $('#trip-details');
   tripDetails.empty();
-  console.log("rendering one trip");
   const oneTrip = new Trip({id: id});
-  console.log(oneTrip);
   oneTrip.fetch({}).done(() => {
     tripDetails.append(showDetailsTemplate(oneTrip.attributes));
   });
-  console.log(oneTrip);
-  // oneTrip.on('change', renderDetails);
-
-  // let tripHTML = showDetailsTemplate(oneTrip.attributes);
-  // tripDetails.append( $(tripHTML));
 }; // renderDetails
 
 
+const makeReservation = function makeReservation() {
+const resData = {};
+RES_FIELDS.forEach((field) => {
+  const inputElement = $(`#reservation-form input[name="{ field }"]`);
+  resData[field] = inputElement.val();
+});
 
+return resData;
+
+};
+
+const clearForm = function clearForm() {
+  $('#reservation-form input[name]').val('');
+}
+
+
+// Document Ready
 $(document).ready( () => {
+
 
   allTripsTemplate = _.template($('#all-trips-template').html() );
   showDetailsTemplate = _.template($('#show-details-template').html() );
 
+
+
+  $('#individual-trip').hide();
+  $('#all-trips').hide();
+  $('#reserve-trip').hide();
+
   const tripList = new TripList();
-  // let oneTrip = new Trip();
 
   tripList.on('sort', renderAll);
 
+  //Events
 
-//
-// model.on('change', this.render, this) or model.on({change: this.render}, this)
-  //Event handlers for buttons
+  // // make reservations
+  // $('.trip-details').on('click', 'h4', function() {
+  //   let tripID = $(this).attr('data-id');
+  //   $('#book-trip-form').attr("data-id", tripID);
+  //   $('#book-trip-form').show();
+  // })
+  //
+  // $('#book-trip-form').on('submit', function(event) {
+  //   event.preventDefault();
+  //   let tripID = $(this).attr('data-id');
+  //   let formData = $('#book-trip-form').serialize();
+  //   reserveTrip(tripID, formData);
+  // }) //end book trip
+
+  // book trip
+$('#trip-details').on('click', 'h4', function() {
+  console.log("You clicked to see the res form");
+  console.log("this");
+  const bookTripID = $(this).attr('data-id');
+  $('#reservation-form').attr("data-id", bookTripID);
+  $('#reservation-form').show();
+})
+
+  //  show trip details
   $('#trips-list').on('click', '.trips', (event) => {
-    console.log("you did it ");
-    console.log(event.target);
     const tripID = $(event.target).attr('data-id');
-    console.log(tripID);
-    console.log("about to render details");
+    $('#individual-trip').show();
     renderDetails(tripID);
   });
 
+  // show all the trips
 
   const allTrips = $('#load-trips');
   allTrips.on('click', () => {
     tripList.fetch();
-    console.log("I fetched it!");
-    // tripList.renderAll;
+    $('#all-trips').show();
   })
 
-  $('#testing').on('click', sampleHandler);
 
 
 
-  //Event handlers for table headers
+
+  // Sort Fields
   TRIP_FIELDS.forEach((field) => {
     const headerElement = $(`.sort.${ field }`);
 
