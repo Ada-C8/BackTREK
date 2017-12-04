d5c50386ee8fb9ec2eaaf7f58346d361719a3dc9
Canaan West <canaan.west@gmail.com>
2017-12-03 17:27:47 -0800

    adding initialize

--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -2,6 +2,19 @@ import Backbone from 'backbone';
 
 const Trip = Backbone.Model.extend({
   urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips/',
+  defaults: {
+    about: "n/a",
+  },
+
+  initialize: function(attributes) {
+    console.log("initializing: attributes:")
+    console.log(attributes)
+  },
+
+  validate: function(attributes){
+
+  },
+
 });
 
 export default Trip
