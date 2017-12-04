5a621779ca0d19022aa3532c311143dc0fbc4088
Canaan West <canaan.west@gmail.com>
2017-12-03 18:25:49 -0800

    cleaning up comments

--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -5,7 +5,6 @@ const TripList = Backbone.Collection.extend({
   model: Trip,
   url: 'https://ada-backtrek-api.herokuapp.com/trips',
   parse(response) {
-    // console.log(response);
   return response;
   }
 })
--- src/app/models/reservation.js
+++ src/app/models/reservation.js
@@ -6,11 +6,6 @@ const Reservation = Backbone.Model.extend({
     console.log(attributes);
     this.url = 'https://ada-backtrek-api.herokuapp.com/trips/' + attributes.trip_id + '/reservations'
   }
-  // urlRoot: 'https://ada-backtrek-api.herokuapp.com/trips/',
-  // url: function(){
-  //   let url = this.urlRoot + this.id + '/reservations'
-  //   return url
-  // }
 });
 
 export default Reservation
